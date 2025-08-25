f1472e71edf660115aea010431b70a2f5e1b5b73
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-25 14:12:18 +0530

    add dto layer for valid signup and login response

---
+++ src/main/java/com/gym/authservice/Dto/Request/SignupRequestDto.java
@@ -0,0 +1,40 @@
+package com.gym.authservice.Dto.Request;
+
+import com.gym.authservice.Roles.PaymentMode;
+import com.gym.authservice.Roles.RoleType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignupRequestDto {
+    @NotBlank(message = "firstName is required")
+    private String  firstName;
+    @NotBlank(message = "LastName is required")
+    private String  lastName;
+    @Email (message = "Enter a valid email id")
+    @NotBlank(message = "email can not be empty")
+    private String email;
+   @NotBlank (message = "phone no is required")
+   @Size(min = 10,max = 12,message = "Phone no can be between 10 to 12 digits")
+    private String phone;
+   @NotBlank(message = "password is required")
+   @Size(min = 6, message = "minimum 6 characters is needed")
+    private String password;
+   @NotBlank(message = "Gender is required")
+   private String gender;
+   @NotBlank(message = "Please Select a role to signUp")
+    private RoleType role;
+   @NotBlank(message = "joining date can not be empty")
+    private LocalDate joinDate;
+    @NotBlank(message = "select a payment mode")
+    private PaymentMode firstPayment;
+}
---
+++ src/main/java/com/gym/authservice/Dto/Response/SignUpResponseDto.java
@@ -0,0 +1,8 @@
+package com.gym.authservice.Dto.Response;
+
+import lombok.Data;
+
+@Data
+public class SignUpResponseDto {
+    private String message;
+}
