55062a53c58d5c9cbac7b0e4f8e391e8b4a6c232
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-25 14:14:50 +0530

    add custom user detalis service and jwt filter and util for json webstoken

---
+++ src/main/java/com/gym/authservice/Security/Jwt/JwtUtil.java
@@ -0,0 +1,66 @@
+package com.gym.authservice.Security.Jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+public class JwtUtil {
+    @Value("${spring.secret.key}")
+    private String secretKey;
+    private final long expiration = 1000 * 60 * 60;
+
+    private SecretKey getKey() {
+        byte[] bytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return
+                Jwts.builder()
+                        .setSubject(email)
+                        .claim("role", role)
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                        .signWith(getKey())
+                        .compact();
+    }
+
+    public Claims extractAllClaims(String token) {
+        return
+                Jwts
+                        .parserBuilder()
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+    }
+
+    public String extractEmail(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date getExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    public boolean isExpired(String token) {
+        return getExpiration(token).before(new Date());
+    }
+
+    public boolean isValidToken(String token) {
+        boolean expired = false;
+        if (!isExpired(token)) {
+            try {
+                extractAllClaims(token);
+                expired = true;
+            } catch (JwtException e) {
+                expired = false;
+            }
+        }
+        return expired;
+    }
+}
