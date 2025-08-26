ff8c91c37716ff57cebd7a05904d02fc20715ce3
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-26 17:02:10 +0530

    add controller for signup

---
+++ src/main/java/com/gym/authservice/Controller/SignUpController.java
@@ -0,0 +1,26 @@
+package com.gym.authservice.Controller;
+
+import com.gym.authservice.Dto.Request.SignupRequestDto;
+import com.gym.authservice.Dto.Response.SignUpResponseDto;
+import com.gym.authservice.Service.SignUpService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/fitStudio/auth/")
+@Validated
+public class SignUpController {
+
+    private final SignUpService signUpService;
+    @PostMapping("signup")
+    public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignupRequestDto requestDto){
+        return  ResponseEntity.ok(signUpService.signUp(requestDto));
+    }
+}
