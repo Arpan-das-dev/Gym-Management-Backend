ad7835b6ef2203745a031d058e4cd953781923ba
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-26 17:00:20 +0530

    add response dtos

--- src/main/java/com/gym/authservice/Dto/Response/SignUpResponseDto.java
+++ src/main/java/com/gym/authservice/Dto/Response/SignUpResponseDto.java
@@ -1,8 +1,10 @@
 package com.gym.authservice.Dto.Response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SignUpResponseDto {
     private String message;
 }
