579e5a36aa907994e68a1a9537872f8416647e15
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-26 17:01:21 +0530

    add response dto for notifiaction service

---
+++ src/main/java/com/gym/authservice/Dto/Response/SignupNotificationDto.java
@@ -0,0 +1,12 @@
+package com.gym.authservice.Dto.Response;
+
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class SignupNotificationDto {
+    private String id;
+    private String email;
+    private String phone;
+    private String name;
+}
