bdae03e96fb06ce1e0b01da190b261ce4b80c6c1
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-28 20:15:25 +0530

    add repository layer

--- src/main/java/com/gym/authservice/Repository/SignedUpsRepository.java
+++ src/main/java/com/gym/authservice/Repository/SignedUpsRepository.java
@@ -2,12 +2,13 @@ package com.gym.authservice.Repository;
 
 import com.gym.authservice.Entity.SignedUps;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-@Repository
+
 public interface SignedUpsRepository extends JpaRepository<SignedUps,String > {
     boolean existsByEmail(String email);
-
+    boolean existsByPhone(String phone);
    Optional< SignedUps> findByEmail(String username);
+
+    Optional<SignedUps> findByPhone(String key);
 }
