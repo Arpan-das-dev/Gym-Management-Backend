49533174112f4dbecc6bd738987cdd68dde871fc
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-28 20:14:36 +0530

    add otp verification service

--- src/main/java/com/gym/authservice/Service/VerificationService.java
+++ src/main/java/com/gym/authservice/Service/VerificationService.java
@@ -1,37 +1,68 @@
 package com.gym.authservice.Service;
 
-
+import com.gym.authservice.Entity.SignedUps;
+import com.gym.authservice.Repository.SignedUpsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
-public class OtpService {
+public class VerificationService {
+
     private final StringRedisTemplate redisTemplate;
+    private final SignedUpsRepository signedUpsRepository;
 
-    public void StoreEmailOtp(String key,String otp,long ttlSeconds){
-        redisTemplate.opsForValue().set("OTP:EMAIL:"+key,otp,ttlSeconds, TimeUnit.SECONDS);
-    }
-    public void StorePhoneOtp(String key,String otp,long ttlSeconds){
-        redisTemplate.opsForValue().set("OTP:PHONE"+key,otp,ttlSeconds,TimeUnit.SECONDS);
+    public void StoreEmailOtp(String key, String otp, long ttlSeconds) {
+        redisTemplate.opsForValue().set("OTP:EMAIL:" + key, otp, ttlSeconds, TimeUnit.SECONDS);
     }
 
-    public boolean verifyEmail(String key,String otp){
-        String value = redisTemplate.opsForValue().get("OTP:EMAIL"+key);
-        return otp !=null && otp.equals(value);
-    }
-    public boolean verifyPhone(String key,String otp){
-        String value = redisTemplate.opsForValue().get("OTP:PHONE"+key);
-        return otp != null && otp.equals(value);
+    public void StorePhoneOtp(String key, String otp, long ttlSeconds) {
+        redisTemplate.opsForValue().set("OTP:PHONE:" + key, otp, ttlSeconds, TimeUnit.SECONDS);
     }
 
-    public void deleteEmailOtp(String key){
-        redisTemplate.delete("OTP:EMAIL"+key);
+
+    @Transactional
+    public boolean verifyEmail(String key, String otp) {
+        if (signedUpsRepository.existsByEmail(key)) {
+            String value = redisTemplate.opsForValue().get("OTP:EMAIL:" + key);
+            if (otp != null && otp.equals(value)) {
+                deleteEmailOtp(key);
+                SignedUps user = signedUpsRepository.findByEmail(key)
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+                user.setEmailVerified(true);
+                return true;
+            }
+        }
+        return false;
     }
-    public void deletePhoneOtp(String key){
-        redisTemplate.delete("OTP:PHONE"+key);
+
+    @Transactional
+    public boolean verifyPhone(String key, String otp) {
+        SignedUps user = signedUpsRepository.findByPhone(key)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!user.isEmailVerified()) {
+            throw new RuntimeException("Please verify email before verifying phone");
+        }
+
+        String value = redisTemplate.opsForValue().get("OTP:PHONE:" + key);
+        if (otp != null && otp.equals(value)) {
+            deletePhoneOtp(key);
+            user.setPhoneVerified(true);
+            return true;
+        }
+        return false;
+    }
+
+    public void deleteEmailOtp(String key) {
+        redisTemplate.delete("OTP:EMAIL:" + key);
+    }
+
+    public void deletePhoneOtp(String key) {
+        redisTemplate.delete("OTP:PHONE:" + key);
     }
 }
