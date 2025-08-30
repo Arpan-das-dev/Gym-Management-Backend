3707878dc081b94b56eef6a7a37b841c7401a5dc
Arpan-das-dev <dasarpan915@gmail.com>
2025-08-30 21:39:33 +0530

    add Response Dto

--- src/main/java/com/gym/authservice/Dto/Response/PhoneOtpNotificationDto.java
+++ src/main/java/com/gym/authservice/Dto/Response/PhoneOtpNotificationDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class LoginResponseDto {
-    private String token;
-    private String role;
+public class PhoneOtpNotificationDto {
+    private String phone;
+    private String otp;
 }
--- src/main/java/com/gym/authservice/Dto/Response/ResetPasswordResponseDto.java
+++ src/main/java/com/gym/authservice/Dto/Response/ResetPasswordResponseDto.java
@@ -1,4 +1,8 @@
 package com.gym.authservice.Dto.Response;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ResetPasswordResponseDto {
+    private String message;
 }
