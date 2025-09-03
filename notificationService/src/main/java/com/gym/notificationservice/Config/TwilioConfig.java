b043b430892e09e1f695e4a7592e89b4a7305fee
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-03 15:13:16 +0530

    add twilo config

--- notificationService/src/main/java/com/gym/notificationservice/Config/TwilioConfig.java
+++ notificationService/src/main/java/com/gym/notificationservice/Config/TwilioConfig.java
@@ -1,7 +1,22 @@
 package com.gym.notificationservice.Config;
 
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class TwiliowConfig {
+public class TwilioConfig {
+
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+    @PostConstruct
+    public void init(){
+        Twilio.init(accountSid,authToken);
+    }
+
 }
