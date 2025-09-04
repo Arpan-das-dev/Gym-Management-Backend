e7dcf629a56bff0369dfc3f9d72c14d27d9b0f50
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:27:41 +0530

    add response Dtos

--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/SignupResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/SignupResponseDto.java
@@ -1,4 +1,37 @@
 package com.gym.adminservice.Dto.Responses;
 
+import com.gym.adminservice.Enums.RoleType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class SignupResponseDto {
+    @NotBlank(message = "firstName is required")
+    private String  firstName;
+    @NotBlank(message = "LastName is required")
+    private String  lastName;
+    @Email(message = "Enter a valid email id")
+    @NotBlank(message = "email can not be empty")
+    private String email;
+    @NotBlank (message = "phone no is required")
+    @Size(min = 10,max = 12,message = "Phone no can be between 10 to 12 digits")
+    private String phone;
+    @NotBlank(message = "password is required")
+    @Size(min = 6, message = "minimum 6 characters is needed")
+    private String password;
+    @NotBlank(message = "Gender is required")
+    private String gender;
+    @NotNull(message = "Please Select a role to signUp")
+    private RoleType role;
+    @NotNull(message = "joining date can not be empty")
+    private LocalDate joinDate;
 }
