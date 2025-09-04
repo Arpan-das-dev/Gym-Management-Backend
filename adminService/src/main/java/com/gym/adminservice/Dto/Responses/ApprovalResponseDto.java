7f1c95f87d41b577f24131b95deb9ffb9ca1cee0
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:27:41 +0530

    add response Dtos

--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/ApprovalResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/ApprovalResponseDto.java
@@ -1,4 +1,13 @@
 package com.gym.adminservice.Dto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ApprovalResponseDto {
+    private String email;
+    private boolean approval;
 }
--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/ApproveEmailNotificationDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/ApproveEmailNotificationDto.java
@@ -1,4 +1,15 @@
 package com.gym.adminservice.Dto.Responses;
 
-public class EmailNotifcationDto {
+import com.gym.adminservice.Enums.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ApproveEmailNotificationDto {
+    private String email;
+    private String name;
+    private RoleType role;
 }
