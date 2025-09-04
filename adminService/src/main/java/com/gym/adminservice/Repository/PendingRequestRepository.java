d7d77e67765a78e84f48198d341aab267caeca7b
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:29:40 +0530

    add repositories

--- adminService/src/main/java/com/gym/adminservice/Repository/PendingRequestRepository.java
+++ adminService/src/main/java/com/gym/adminservice/Repository/PendingRequestRepository.java
@@ -1,4 +1,14 @@
 package com.gym.adminservice.Repository;
 
-public class PendingRequestRepository {
+import com.gym.adminservice.Models.PendingRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PendingRequestRepository extends JpaRepository<PendingRequest, String> {
+    void deleteByEmail(String email);
+
+    Optional<PendingRequest> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
