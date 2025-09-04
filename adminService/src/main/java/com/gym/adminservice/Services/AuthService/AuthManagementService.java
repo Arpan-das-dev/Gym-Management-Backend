578d851342144cbb4d263ba54e9b8d2018dd1728
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:31:16 +0530

    add services for AuthService

--- adminService/src/main/java/com/gym/adminservice/Services/AuthService/AuthManagementService.java
+++ adminService/src/main/java/com/gym/adminservice/Services/AuthService/AuthManagementService.java
@@ -1,22 +1,28 @@
-package com.gym.adminservice.Services;
+package com.gym.adminservice.Services.AuthService;
 
 
 
+import com.gym.adminservice.Dto.Requests.CreateAdminRequestDto;
 import com.gym.adminservice.Dto.Requests.CreateMemberRequestDto;
 import com.gym.adminservice.Dto.Requests.CreateTrainerRequestDto;
 import com.gym.adminservice.Dto.Responses.MemberResponseDto;
 import com.gym.adminservice.Dto.Responses.SignupResponseDto;
 import com.gym.adminservice.Dto.Responses.TrainerResponseDto;
 import com.gym.adminservice.Enums.RoleType;
+import com.gym.adminservice.Models.AdminEntity;
+import com.gym.adminservice.Repository.AdminRepository;
+import com.gym.adminservice.Services.WebClientServices.WebClientAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.management.relation.RoleInfo;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class AuthManagementService {
     private final WebClientAuthService webClientService;
+    private final AdminRepository adminRepository;
 
     public String createMember(CreateMemberRequestDto memberRequestDto) {
         SignupResponseDto responseDto = SignupResponseDto.builder()
@@ -36,7 +42,7 @@ public class AuthManagementService {
                 .build();
 
         webClientService.sendSignupDetailsMember(responseDto);
-        webClientService.sendMemberDetails(memberResponseDto);
+        // webClientMemberService.sendMemberDetails(memberResponseDto)
 
         return "Member created successfully";
     }
@@ -61,21 +67,69 @@ public class AuthManagementService {
                 .build();
 
         webClientService.sendSignupDetailsTrainer(responseDto);
-        webClientService.sendTrainerDetails(trainerResponseDto);
+        //webClientTrainerService.sendTrainerDetails(trainerResponseDto)
 
         return "Trainer created successfully";
     }
 
-    public String deleteUser(String identifier, RoleInfo role){
-        if(role.getName().contains("TRAINER")){
+    @Transactional
+    public String createAdmin(CreateAdminRequestDto requestDto){
+        SignupResponseDto responseDto = SignupResponseDto.builder()
+                .email(requestDto.getEmail())
+                .role(RoleType.ADMIN_ADMIN)
+                .firstName(requestDto.getFirstName()).lastName(requestDto.getLastName())
+                .gender(requestDto.getGender())
+                .joinDate(requestDto.getJoinDate())
+                .password(requestDto.getPassword())
+                .build();
+        AdminEntity model = AdminEntity.builder()
+                .id(String.valueOf(UUID.randomUUID()))
+                .firstName(requestDto.getFirstName())
+                .lastName(requestDto.getLastName())
+                .email(requestDto.getEmail())
+                .phone(requestDto.getPhone())
+                .joinDate(requestDto.getJoinDate())
+                .gender(requestDto.getGender())
+                .role(requestDto.getRole())
+                .build();
+
+        adminRepository.save(model);
+        webClientService.sendSignupDetailsAdmin(responseDto);
+
+        return "Admin created successfully";
+    }
+
+    public String setCustomIdToAdmin(String id, String role, String email) {
+        AdminEntity entity = adminRepository.findByEmail(email).
+                orElseThrow(); //userNotFoundException
+        entity.setId(id);
+        return "Id set successfully";
+    }
+
+    public String deleteUser(String identifier, RoleType role){
+        if(role.isTrainerRole()){
             webClientService.deleteUser(identifier);
-            // webClientTrainerService.deleteTrainer(identifier)
+            // webClientTrainerService.deleteTrainer(identifier);
             return "trainer request deleted successfully";
         } else {
-            // webClientMemberService.deleteMember(identifier)
+            webClientService.deleteUser(identifier);
+            // webClientMemberService.deleteMember(identifier);
             return "member request deleted successfully";
         }
     }
+
+    public String freezeAccount(String email,RoleType role){
+        if(role.isTrainerRole()){
+            //webClientTrainerService.freezeAccount(email,role)
+            return "Trainer Account frozen Successfully";
+        } else if (role.equals(RoleType.MEMBER)) {
+            //webClientMemberService.freezeAccount(identifier);
+            return "Member Account frozen Successfully";
+        } else{
+            return "account frozen successfully";
+        }
+    }
+
 
 
 }
