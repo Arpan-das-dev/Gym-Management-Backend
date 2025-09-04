4256125f2d195063bf49c476e71bee08052c0227
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:29:04 +0530

    add Entities

--- adminService/src/main/java/com/gym/adminservice/Models/AdminEntity.java
+++ adminService/src/main/java/com/gym/adminservice/Models/AdminEntity.java
@@ -1,4 +1,45 @@
 package com.gym.adminservice.Models;
 
+import com.gym.adminservice.Enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "admins")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AdminEntity {
+
+    @Id
+    @Column(name = "admin_id",nullable = false,unique = true)
+    private String id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 12, unique = true)
+    private String phone;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RoleType role = RoleType.ADMIN;
+
+    @Column(nullable = false)
+    private LocalDate joinDate;
 }
