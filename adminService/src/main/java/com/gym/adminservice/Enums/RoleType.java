cb8dc3e42a7d005a84e834f85e288fb9497c8075
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-04 21:28:20 +0530

    add Roles

--- adminService/src/main/java/com/gym/adminservice/Enums/RoleType.java
+++ adminService/src/main/java/com/gym/adminservice/Enums/RoleType.java
@@ -1,4 +1,12 @@
-package com.gym.adminservice;
+package com.gym.adminservice.Enums;
 
-public class RoleType {
+public enum RoleType {
+    ADMIN,
+    MEMBER,
+    TRAINER,
+    TRAINER_PENDING,
+    TRAINER_ADMIN, ADMIN_ADMIN;
+    public boolean isTrainerRole() {
+        return this == TRAINER || this == TRAINER_PENDING || this == TRAINER_ADMIN;
+    }
 }
