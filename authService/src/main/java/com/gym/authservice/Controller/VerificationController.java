35f80e4301e61f3019d7c48ed8ad35a12315c5b3
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:28:37 +0530

    kdgdkgh

--- authService/src/main/java/com/gym/authservice/Controller/VerificationController.java
+++ authService/src/main/java/com/gym/authservice/Controller/VerificationController.java
@@ -18,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${authService.base_url}")
 @RequiredArgsConstructor
 @Validated
+/*
+ * This controller manages the verification of user email addresses and phone numbers using OTPs.
+ * It provides endpoints for verifying email and phone OTPs.
+ * The controller utilizes VerificationService to handle the verification logic.
+ */
 public class VerificationController {
 
     private final VerificationService verificationService;
 
+    /*
+     * Endpoint to verify email using OTP.
+     * Accepts an EmailVerificationRequestDto containing the email and OTP.
+     * Returns a success message if the OTP is valid, otherwise returns an error message.
+     */
     @PostMapping("verifyEmail")
     public ResponseEntity<String> verifyEmail(@Valid @RequestBody EmailVerificationRequestDto requestDto) {
         if (verificationService.verifyEmail(requestDto.getEmail(), requestDto.getOtp())) {
@@ -30,6 +40,10 @@ public class VerificationController {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid OTP");
     }
 
+    /* Endpoint to verify phone using OTP.
+     * Accepts a PhoneVerificationRequestDto containing the phone number and OTP.
+     * Returns a success message if the OTP is valid, otherwise returns an error message.
+     */
     @PostMapping("verifyPhone")
     public ResponseEntity<String> verifyPhone(@Valid @RequestBody PhoneVerificationRequestDto requestDto) {
         boolean verification = verificationService.verifyPhone(requestDto.getPhone(), requestDto.getOtp());
