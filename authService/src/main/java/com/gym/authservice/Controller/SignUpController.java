610660bdff54b7fcd684ebef7e89e5ce45a8ba4d
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:25:24 +0530

    add Signup Controller

--- authService/src/main/java/com/gym/authservice/Controller/SignUpController.java
+++ authService/src/main/java/com/gym/authservice/Controller/SignUpController.java
@@ -5,23 +5,50 @@ import com.gym.authservice.Dto.Response.SignUpResponseDto;
 import com.gym.authservice.Service.SignUpService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("${authService.base_url}")
 @Validated
+/*
+ * This controller handles user sign-up operations, including sending OTPs via email and phone.
+ * It provides endpoints for user registration and OTP delivery.
+ * The controller uses SignUpService to process the sign-up logic and manage OTPs for verification.
+ */
 public class SignUpController {
 
     private final SignUpService signUpService;
 
+    /*
+     * Endpoint to handle user sign-up requests.
+     * Accepts a SignupRequestDto containing user details and returns a SignUpResponseDto upon successful registration.
+     */
     @PostMapping("signup")
     public ResponseEntity<SignUpResponseDto> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
         return ResponseEntity.ok(signUpService.signUp(requestDto));
+    }
+
+    /* Endpoint to send OTP via email 
+     * Accepts an email address as a path variable and triggers the sending of an OTP to that email.
+     * Returns a success message upon successful OTP dispatch.
+    */
+    @PostMapping("signup/{email}")
+    public ResponseEntity<String> sendEmailOtp(@PathVariable String email) {
+        signUpService.sendEmailOtp(email);
+        return ResponseEntity.status(HttpStatus.OK).body("otp sent successfully via email");
+    }
+    
+    /* Endpoint to send OTP via phone
+     * Accepts a phone number as a path variable and triggers the sending of an OTP to that phone number.
+     * Returns a success message upon successful OTP dispatch.
+    */
+    @PostMapping("signup/{phone}")
+    public ResponseEntity<String> sendPhoneOtp(@PathVariable String phone) {
+        signUpService.sendPhoneOtp(phone);
+        return ResponseEntity.status(HttpStatus.OK).body("otp sent successfully via sms");
     }
 }
