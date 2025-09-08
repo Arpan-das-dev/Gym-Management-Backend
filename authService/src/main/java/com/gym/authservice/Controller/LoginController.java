ee77139b2b93e95ad309854fdb9827b6a1b0d8a5
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:30:50 +0530

    add login controller

--- authService/src/main/java/com/gym/authservice/Controller/LoginController.java
+++ authService/src/main/java/com/gym/authservice/Controller/LoginController.java
@@ -17,15 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("${authService.base_url}")
 @RequiredArgsConstructor
 @Validated
+
+/*
+ * This controller handles user login operations and account deletion verification.
+ * It provides endpoints for user authentication and verifying credentials before account deletion.
+ * The controller uses LogiInService to process login requests and manage verification logic.
+ */
 public class LoginController {
 
     private final LogiInService logiInService;
 
+    /*
+     * Endpoint to handle user login requests.
+     * Accepts a LogiInRequestDto containing user credentials and returns a LoginResponseDto upon successful authentication.
+     * If authentication fails, an appropriate error response is returned.
+     */
     @PostMapping("login")
     public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LogiInRequestDto requestDto){
         LoginResponseDto responseDto = logiInService.login(requestDto);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
+
+    /*
+     * Endpoint to verify user credentials before account deletion.
+     * Accepts a LogiInRequestDto containing user credentials and returns a boolean indicating whether the credentials are valid.
+     * This verification step is crucial to ensure that only authorized users can delete their accounts.
+     */
     @PostMapping("delete/verify")
     public ResponseEntity<Boolean> deleteAccountVerification (@Valid @RequestBody LogiInRequestDto requestDto){
         Boolean response = logiInService.verifyBeforeDelete(requestDto);
