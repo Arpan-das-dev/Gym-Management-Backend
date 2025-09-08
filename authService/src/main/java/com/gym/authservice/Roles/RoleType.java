aa360dd919e4c6d5dfcf83ad8fe997c7cf5606af
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:35:21 +0530

    add enums of role

--- authService/src/main/java/com/gym/authservice/Roles/RoleType.java
+++ authService/src/main/java/com/gym/authservice/Roles/RoleType.java
@@ -1,9 +1,12 @@
 package com.gym.authservice.Roles;
-
+/*
+ * this enum represents the different roles available in the gym management system.
+ * Each role defines a specific set of permissions and access levels for users within the system.
+ */
 public enum RoleType {
     ADMIN,
     MEMBER,
     TRAINER,
     TRAINER_PENDING,
-    TRAINER_ADMIN
+    TRAINER_ADMIN, ADMIN_ADMIN
 }
