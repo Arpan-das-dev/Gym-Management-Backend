f6d647d46d6a8c6d2eaf92596a3c04b0618dc74c
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:04:22 +0530

    add webclient config

--- adminService/src/main/java/com/gym/adminservice/Configs/WebClientConfig.java
+++ adminService/src/main/java/com/gym/adminservice/Configs/WebClientConfig.java
@@ -6,7 +6,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
+/*
+ * Bean for WebClient to make HTTP requests to other services
+ */
     @Bean
     public WebClient.Builder webClient(){
         return WebClient.builder();
