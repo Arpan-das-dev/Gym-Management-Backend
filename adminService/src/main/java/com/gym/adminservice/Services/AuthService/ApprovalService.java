d5c94bb7d2e65db4d8a95b69a6a472af9c56cdaa
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:13:09 +0530

    add approval service

--- adminService/src/main/java/com/gym/adminservice/Services/AuthService/ApprovalService.java
+++ adminService/src/main/java/com/gym/adminservice/Services/AuthService/ApprovalService.java
@@ -13,19 +13,38 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+
+/*
+ * this service class will handle also the approval and decline of the request
+ * which
+ * is sent from the auth service when a user signs up as a trainer or member,
+ * and also it will send the approval status to the auth service, also send
+ * email notification and
+ * role regarding services.
+ */
+
 public class ApprovalService {
     private final PendingRequestRepository pendingRequestRepository;
-    private final WebClientAuthService webClientService;
+    private final WebClientAuthService webAuthClientService;
     private final WebClientNotificationService webClientNotificationService;
 
-    public String insertRequest(ApprovalRequestDto requestDto){
-        if(pendingRequestRepository.existsByEmail(requestDto.getEmail())) {
+    /*
+     * this service will insert all those request in the pending request table
+     * which are sent from the auth service when a user signs up as a trainer or
+     * member
+     * and also if the request is already present it will throw an exception
+     */
+
+    public String insertRequest(ApprovalRequestDto requestDto) {
+        if (pendingRequestRepository.existsByEmail(requestDto.getEmail())) {
             throw new RuntimeException("request already present please wait until verified");
         }
         PendingRequest pendingRequest = PendingRequest.builder()
+                .id(String.valueOf(UUID.randomUUID()))
                 .email(requestDto.getEmail())
                 .name(requestDto.getName())
                 .phone(requestDto.getPhone())
@@ -36,9 +55,21 @@ public class ApprovalService {
         return "successfully sent request";
     }
 
-    public List<PendingRequest> getAll(){
+    /*
+     * this service will return all the pending requests from the pending request
+     * table
+     * which we will show in the frontend for the admin to approve or decline
+     */
+
+    public List<PendingRequest> getAll() {
         return pendingRequestRepository.findAll();
     }
+
+    /*
+     * this service will send the approval status to the auth service and also
+     * send email notification to the user regarding the approval status
+     * and also delete the request from the pending request table
+     */
 
     @Transactional
     public ApprovalResponseDto sendApproval(ApprovalRequestDto requestDto) {
@@ -47,33 +78,50 @@ public class ApprovalService {
                 .email(requestDto.getEmail())
                 .approval(true)
                 .build();
-        webClientService.sendApproval(responseDto.getEmail(),responseDto.isApproval());
+        // send the approval status to the auth service
+        webAuthClientService.sendApproval(responseDto.getEmail(), responseDto.isApproval());
+        // send email notification to the user regarding the approval of the request
         webClientNotificationService.sendApproveMail(new ApproveEmailNotificationDto(
-                requestDto.getEmail(),requestDto.getName(),requestDto.getRole()
-        ));
+                requestDto.getEmail(), requestDto.getName(), requestDto.getRole()));
         return responseDto;
     }
 
+    /*
+     * this service will decline the approval request and also delete the user from
+     * the auth service
+     * and also delete the trainer or member details from their respective services
+     */
+
     @Transactional
-    public ApprovalResponseDto declineApproval(ApprovalRequestDto requestDto){
+    public ApprovalResponseDto declineApproval(ApprovalRequestDto requestDto) {
         PendingRequest request = pendingRequestRepository.findByEmail(requestDto.getEmail())
                 .orElseThrow(); // user not found exception
         pendingRequestRepository.delete(request);
-        if(request.getRole().equals(RoleType.TRAINER_PENDING )){
-            webClientService.deleteTrainer(request.getEmail());
-        } else if (request.getRole().equals(RoleType.MEMBER)) {
-            webClientService.deleteMember(request.getEmail());
-        }
-        webClientService.deleteUser(requestDto.getEmail());
-        webClientNotificationService.sendDeclinedMail(new ApproveEmailNotificationDto
-                (requestDto.getEmail(),requestDto.getName(),requestDto.getRole())
-        );
+        /*
+         * if the user role is trainer then delete the trainer details from the trainer
+         * service's database
+         * if the user role is member then delete the member details from the member
+         * service's database,
+         * but we will implement this later when we will implement the trainer and member services
+         */
+        // if (request.getRole().equals(RoleType.TRAINER_PENDING)) {
+        //     // delete the trainer details from trainer service's database
+        //     webAuthClientService.deleteTrainer(requestDto.getEmail());
+        // } else if (request.getRole().equals(RoleType.MEMBER)) {
+        //     // delete the member details from member service's database
+        //     webAuthClientService.deleteMember(requestDto.getEmail());
+        // }
+        /*
+         * now delete the user from the auth service's database
+         */
+        webAuthClientService.deleteUser(requestDto.getEmail());
+        // send email notification to the user regarding the decline of the request
+        webClientNotificationService.sendDeclinedMail(
+                new ApproveEmailNotificationDto(requestDto.getEmail(), requestDto.getName(), requestDto.getRole()));
         return ApprovalResponseDto.builder()
                 .email(requestDto.getEmail())
                 .approval(false)
                 .build();
     }
-
-
 
 }
