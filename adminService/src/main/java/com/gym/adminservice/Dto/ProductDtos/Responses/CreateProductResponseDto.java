d25ba18ce9d7dc007d465352d8823e727ce17862
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:06:53 +0530

    add Responses to send via webclient

--- adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/CreateProductResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/CreateProductResponseDto.java
@@ -1,4 +1,17 @@
 package com.gym.adminservice.Dto.ProductDtos.Responses;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@Data
 public class CreateProductResponseDto {
+    private String productCode;
+    private String description;
+    private List<ProductFlavourResponseDto> flavours;
 }
