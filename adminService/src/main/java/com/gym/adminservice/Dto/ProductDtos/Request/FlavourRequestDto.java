7c9bb0f48ae365c5fc94675176021728099da833
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:06:06 +0530

    add dtos required for valid product creatiion

--- adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Request/FlavourRequestDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Request/FlavourRequestDto.java
@@ -1,4 +1,22 @@
 package com.gym.adminservice.Dto.ProductDtos.Request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+@AllArgsConstructor
 public class FlavourRequestDto {
+    @NotBlank(message = "Flavour name cannot be empty")
+    private String flavourName;
+
+    @NotEmpty(message = "Select at least one product variant")
+    @Valid
+    private List<VariantRequestDto> variants;
 }
