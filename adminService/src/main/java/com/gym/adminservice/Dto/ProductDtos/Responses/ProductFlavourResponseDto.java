4402f85f726b34d599cbe86b205195229c6057e9
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-08 13:06:53 +0530

    add Responses to send via webclient

--- adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/ProductFlavourResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/ProductFlavourResponseDto.java
@@ -1,4 +1,18 @@
 package com.gym.adminservice.Dto.ProductDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductFlavourResponseDto {
+    private String productId;
+    private String flavourName;
+    private List<ProductVariantResponseDto> variantResponseDto;
 }
--- adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/ProductVariantResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/ProductDtos/Responses/ProductVariantResponseDto.java
@@ -1,4 +1,19 @@
 package com.gym.adminservice.Dto.ProductDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductVariantResponseDto {
+    private Integer mrp;
+    private Integer sellingPrice;
+    private Integer weight;
+    private String  unit;
+    private Integer quantity;
+    private String url;
 }
