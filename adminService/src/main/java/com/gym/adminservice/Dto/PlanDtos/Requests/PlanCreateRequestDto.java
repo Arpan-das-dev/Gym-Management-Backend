f1f717cda6357239acba9e5c0863ffd85aae6718
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-09 18:57:07 +0530

    add plan request dto

--- adminService/src/main/java/com/gym/adminservice/Dto/PlanDtos/Requests/PlanCreateRequestDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/PlanDtos/Requests/PlanCreateRequestDto.java
@@ -1,4 +1,26 @@
 package com.gym.adminservice.Dto.PlanDtos.Requests;
 
-public class PlanCreateRequest {
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class PlanCreateRequestDto {
+    @NotBlank(message = "Plan name is required")
+    private String planName;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    private Double price;
+
+    @NotNull(message = "Duration is required")
+    @Min(value = 1, message = "Duration must be at least 1")
+    private Integer duration;
+
+    @NotNull(message = "Features list cannot be null")
+    @Size(min = 1, message = "There must be at least one feature")
+    private List<@NotBlank(message = "Feature cannot be blank") String> features;
 }
