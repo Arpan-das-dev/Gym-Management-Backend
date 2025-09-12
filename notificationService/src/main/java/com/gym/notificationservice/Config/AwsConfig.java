cacd6a1b93955e61ca35a7afb11b504953add4ac
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-12 16:12:25 +0530

    add conigs

--- notificationService/src/main/java/com/gym/notificationservice/Config/AwsConfig.java
+++ notificationService/src/main/java/com/gym/notificationservice/Config/AwsConfig.java
@@ -2,12 +2,12 @@ package com.gym.notificationservice.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+/**
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.sns.SnsClient;
-
+*/
 //@Configuration
 public class AwsConfig {
     /*
