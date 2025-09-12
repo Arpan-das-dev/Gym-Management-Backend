a44a754a080f1e43f9d74a6d653c659e32586c99
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-12 15:58:55 +0530

    add controllers

--- memberService/src/main/java/com/gym/member_service/Controllers/MemberProfileController.java
+++ memberService/src/main/java/com/gym/member_service/Controllers/MemberProfileController.java
@@ -1,4 +1,32 @@
 package com.gym.member_service.Controllers;
 
-public class MemberProfileService {
+import com.gym.member_service.Services.MemberProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("${member-service.Base_Url.profile}")
+@RequiredArgsConstructor
+@Validated
+public class MemberProfileController {
+
+    private final MemberProfileService profileService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadImage(@RequestParam String id,
+                                              @RequestParam("image") MultipartFile image){
+        String urlResponse =  profileService.uploadImage(id,image);
+        return ResponseEntity.accepted().body(urlResponse);
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteImage(@RequestParam String id){
+        profileService.deleteImage(id);
+        return ResponseEntity.accepted().body("Image deleted Successfully");
+    }
+
+
 }
--- memberService/src/main/java/com/gym/member_service/Controllers/MembersCountController.java
+++ memberService/src/main/java/com/gym/member_service/Controllers/MembersCountController.java
@@ -1,6 +1,6 @@
 package com.gym.member_service.Controllers;
 
-import com.gym.member_service.Services.MembersOtherService;
+import com.gym.member_service.Services.MembersCountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.*;
 @Service
 @RestController
 @RequestMapping("${member-service.Base_Url.count}")
-public class MembersOtherController {
-    private final MembersOtherService otherService;
+public class MembersCountController {
+    private final MembersCountService otherService;
 
     @PostMapping("/increment")
     public ResponseEntity<?> markAsActive(@RequestParam String id){
@@ -22,11 +22,11 @@ public class MembersOtherController {
 
     @PostMapping("/decrement")
     public ResponseEntity<?> markAsInactive(@RequestParam String id){
-        otherService.marAsInactive(id);
+        otherService.markAsInactive(id);
         return ResponseEntity.accepted().build();
     }
 
-    @GetMapping("/ActiveCount")
+    @GetMapping("/active-count")
     public ResponseEntity<Long> getActiveMemberCount(){
         return ResponseEntity.status(HttpStatus.OK).body(
                 otherService.getActiveMembersCount()
