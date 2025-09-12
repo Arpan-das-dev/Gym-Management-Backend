09cd6ac8c32a5bcdaa35638a8d263d5299c90e66
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-12 15:55:38 +0530

    add models

--- memberService/src/main/java/com/gym/member_service/Model/MembersActive.java
+++ memberService/src/main/java/com/gym/member_service/Model/MembersActive.java
@@ -1,4 +1,28 @@
 package com.gym.member_service.Model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "membersActive")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MembersActive {
+    @Id
+    private LocalDateTime dateTime;
+
+    @Column(nullable = false)
+    private String time;
+    private long memberCount;
 }
