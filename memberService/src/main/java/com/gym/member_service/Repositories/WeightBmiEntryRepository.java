31659ed5d84365b3907ffd48313899053af1b0cc
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-13 19:58:37 +0530

    add repositories to perform on database by MemberFitService class

--- memberService/src/main/java/com/gym/member_service/Repositories/WeightBmiEntryRepository.java
+++ memberService/src/main/java/com/gym/member_service/Repositories/WeightBmiEntryRepository.java
@@ -2,6 +2,36 @@ package com.gym.member_service.Repositories;
 
 import com.gym.member_service.Model.WeightBmiEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+/*
+ * This repository(interface) is used in memberFitService
+ * to provide essentials(custom) and non essential(inbuild) methods
+ * to do desired operations
+ * @Modifying: use to tell that this method makes some changes in database
+ * @Query: use to add custom query to do database operations
+ * @Param: use to tell that any word in the query is actually dynamic(as method parameter)
+ */
 public interface WeightBmiEntryRepository extends JpaRepository<WeightBmiEntry, Long> {
+
+    Optional<WeightBmiEntry> findByMemberIdAndDate(String memberId, LocalDate date);
+
+    int deleteByMember_IdAndDate(String memberId, LocalDate date);
+
+    /*
+     * a custom method(definition) with custom query to find member with data
+     * between certain date range
+     */
+    @Query(value = "SELECT * FROM members.weight_bmi_entries " +
+            "WHERE member_id = :memberId " +
+            "AND achieved_date BETWEEN :startDate AND :endDate",
+            nativeQuery = true)
+    List<WeightBmiEntry> findMemberByDateRange(@Param("memberId") String memberId,
+                                               @Param("startDate") LocalDate startDate,
+                                               @Param("endDate") LocalDate endDate);
 }
