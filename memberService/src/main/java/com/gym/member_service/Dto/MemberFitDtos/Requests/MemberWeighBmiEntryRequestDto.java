3d55537b3ab21d87f407ca6ef9a9a961481085e8
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-13 20:01:25 +0530

    add request dto for MemberFitService

--- memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Requests/MemberWeighBmiEntryRequestDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Requests/MemberWeighBmiEntryRequestDto.java
@@ -1,4 +1,29 @@
 package com.gym.member_service.Dto.MemberFitDtos.Requests;
 
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class MemberWeighBmiEntryRequestDto {
+
+    @NotNull(message = "date is required")
+    @PastOrPresent(message = "date cannot be in the future")
+    private LocalDate date;
+
+    @NotNull(message = "weight is required")
+    @Positive(message = "weight must be positive")
+    private Double weight;
+
+    @NotNull(message = "bmi is required")
+    @DecimalMin(value = "10.0", message = "bmi must be at least 10")
+    @DecimalMax(value = "50.0", message = "bmi must be at most 50")
+    private Double bmi;
 }
--- memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Requests/PrProgressRequestDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Requests/PrProgressRequestDto.java
@@ -1,4 +1,34 @@
 package com.gym.member_service.Dto.MemberFitDtos.Requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
 public class PrProgressRequestDto {
+
+    @NotNull(message = "Name of workout is required")
+    @NotBlank(message = "Name of workout cannot be blank")
+    @Size(min = 4, max = 40, message = "Workout name should be between 4 and 40 characters")
+    private String workoutName;
+
+    @NotNull(message = "Weight is required")
+    @Positive(message = "Weight must be positive")
+    private Double weight;
+
+    @NotNull(message = "Repetitions is required")
+    @Positive(message = "Repetitions must be positive")
+    private Integer repetitions;
+
+    @NotNull(message = "Achieved date is required")
+    @PastOrPresent(message = "Achieved date cannot be in the future")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate achievedDate;
 }
