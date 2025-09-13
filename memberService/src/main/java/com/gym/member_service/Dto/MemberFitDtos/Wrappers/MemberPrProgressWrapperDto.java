203e1cd7416b94dc21e111f340994615433ecc23
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-13 20:04:29 +0530

    add wrapper arround responses as final response dto for MemberService

--- memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Wrappers/MemberPrProgressWrapperDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Wrappers/MemberPrProgressWrapperDto.java
@@ -1,4 +1,15 @@
 package com.gym.member_service.Dto.MemberFitDtos.Wrappers;
 
+import com.gym.member_service.Dto.MemberFitDtos.Responses.MemberPrProgressResponseDto;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberPrProgressWrapperDto {
+    private List<MemberPrProgressResponseDto> responseDtoList;
 }
