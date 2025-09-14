027934b0200aacfb87ce1cf001a4807d4296009a
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-14 10:09:15 +0530

    add response for output(inside a wrapper) for member fit service

--- memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Responses/MemberWeighBmiEntryResponseDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberFitDtos/Responses/MemberWeighBmiEntryResponseDto.java
@@ -1,4 +1,19 @@
 package com.gym.member_service.Dto.MemberFitDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class MemberWeighBmiEntryResponseDto {
+
+    private LocalDate date;
+    private double bmi;
+    private double weight;
 }
