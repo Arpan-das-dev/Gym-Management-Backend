db7c6a2df7d96a6cf10e68d3b109c74ea4fbf0ec
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-14 10:05:58 +0530

    add custom exception for invalid url

--- memberService/src/main/java/com/gym/member_service/Exception/Exceptions/InvalidImageUrlException.java
+++ memberService/src/main/java/com/gym/member_service/Exception/Exceptions/InvalidImageUrlException.java
@@ -1,4 +1,7 @@
 package com.gym.member_service.Exception.Exceptions;
 
-public class InvalidImageUrlException {
+public class InvalidImageUrlException extends RuntimeException {
+    public InvalidImageUrlException(String message) {
+        super(message);
+    }
 }
