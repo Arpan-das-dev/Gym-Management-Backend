790b2f4272b4facb182ba81e46c6600d3b1221c5
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-14 10:07:11 +0530

    add response dtos for member management service

--- memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Responses/MemberDetailsResponseDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Responses/MemberDetailsResponseDto.java
@@ -1,12 +1,28 @@
-package com.gym.member_service.MemberManagementDto.Responses;
+package com.gym.member_service.Dto.MemberManagementDto.Responses;
+
+import com.gym.member_service.Dto.MemberFitDtos.Responses.DailyRoutineResponseDto;
+import com.gym.member_service.Dto.MemberFitDtos.Responses.MemberPrProgressResponseDto;
+import com.gym.member_service.Dto.MemberFitDtos.Responses.WeightBmiEntryResponseDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @Data
 public class MemberDetailsResponseDto {
+    private String id;
+    private String profileImageUrl;
+    private String name;
+    private String email;
+    private String phone;
+    private boolean frozen;
+    private LocalDateTime planExpiration;
+    private List<WeightBmiEntryResponseDto> weightBmiEntries;
+    private List<MemberPrProgressResponseDto> prProgresses;
+    private List<DailyRoutineResponseDto> dailyRoutines;
 }
