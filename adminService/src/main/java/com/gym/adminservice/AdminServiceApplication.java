72d7c963ba55b6df9c90a8566158e384839f054e
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-18 14:12:06 +0530

    initial commit

--- adminService/src/main/java/com/gym/adminservice/AdminServiceApplication.java
+++ adminService/src/main/java/com/gym/adminservice/AdminServiceApplication.java
@@ -2,10 +2,12 @@ package com.gym.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
+@EnableCaching
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
