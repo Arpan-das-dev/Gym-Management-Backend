4165c1e8a997512a128c88768109419b6761323d
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-18 22:38:03 +0530

    Recover today's TrainerService work

--- authService/src/main/java/com/gym/authservice/Service/VerificationService.java
+++ authService/src/main/java/com/gym/authservice/Service/VerificationService.java
@@ -1,8 +1,11 @@
 package com.gym.authservice.Service;
 
+import com.gym.authservice.Dto.Response.EmailOtpNotificationDto;
+import com.gym.authservice.Dto.Response.PhoneOtpNotificationDto;
 import com.gym.authservice.Entity.SignedUps;
 import com.gym.authservice.Exceptions.Custom.UserNotFoundException;
 import com.gym.authservice.Repository.SignedUpsRepository;
+import com.gym.authservice.Utils.OtpGenerationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,8 @@ public class VerificationService {
 
     private final StringRedisTemplate redisTemplate;
     private final SignedUpsRepository signedUpsRepository;
+    private final OtpGenerationUtil generationUtil;
+    private final WebClientService notificationService;
 
     public void StoreEmailOtp(String key, String otp, long ttlSeconds) {
         redisTemplate.opsForValue().set("OTP:EMAIL:" + key, otp, ttlSeconds, TimeUnit.SECONDS);
@@ -25,6 +30,17 @@ public class VerificationService {
         redisTemplate.opsForValue().set("OTP:PHONE:" + key, otp, ttlSeconds, TimeUnit.SECONDS);
     }
 
+    public void sendEmailOtp(String email,String name) {
+        String otpEmail = generationUtil.generateOtp(6);
+        StoreEmailOtp(email, otpEmail, 900);
+        notificationService.sendEmailOtp(new EmailOtpNotificationDto(email, otpEmail,name));
+    }
+
+    public void sendPhoneOtp(String phone, String name) {
+        String otpPhone = generationUtil.generateOtp(6);
+        StorePhoneOtp(phone, otpPhone, 900);
+        notificationService.sendPhoneOtp(new PhoneOtpNotificationDto(phone, otpPhone,name));
+    }
 
     @Transactional
     public boolean verifyEmail(String key, String otp) {
--- notificationService/src/main/java/com/gym/notificationservice/Service/AuthNotificationService.java
+++ notificationService/src/main/java/com/gym/notificationservice/Service/AuthNotificationService.java
@@ -5,12 +5,31 @@ import com.gym.notificationservice.Dto.AuthNotificationRequests.EmailOtpRequestD
 import com.gym.notificationservice.Dto.AuthNotificationRequests.PhoneOtpRequestDto;
 import com.gym.notificationservice.Dto.AuthNotificationRequests.SendCredentialRequestDto;
 import com.gym.notificationservice.Dto.AuthNotificationRequests.WelcomeRequestDto;
+<<<<<<< Updated upstream
 import lombok.RequiredArgsConstructor;
+=======
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+>>>>>>> Stashed changes
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+<<<<<<< Updated upstream
 /**
+=======
+import java.io.IOException;
+/*
+>>>>>>> Stashed changes
 import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.ses.model.*;
 import software.amazon.awssdk.services.sns.SnsClient;
@@ -25,8 +44,16 @@ public class AuthNotificationService {
     private final SnsClient snsClient;
     */
     private final TemplateEngine templateEngine;
+<<<<<<< Updated upstream
     private final MailService mailService;
     private final SmsService smsService;
+=======
+    private final SendGrid sendGrid;
+    @Value("${app.mail.sender}")
+    private String sender;
+    @Value("${twilio.phone.number}")
+    private String fromNumber;
+>>>>>>> Stashed changes
 
     public void sendWelcomeMail(WelcomeRequestDto requestDto) {
         Context context = new Context();
@@ -34,8 +61,13 @@ public class AuthNotificationService {
         context.setVariable("gymId", requestDto.getId());
         context.setVariable("gmail",requestDto.getEmail());
         context.setVariable("phone",requestDto.getPhone());
+<<<<<<< Updated upstream
         String body = templateEngine.process("welcome-mail", context);
         mailService.sendMail(requestDto.getEmail(), "Welcome to FitStudio", body);
+=======
+        String body = templateEngine.process("Welcome Mail", context);
+        sendMail(requestDto.getEmail(), "Welcome to FitStudio", body);
+>>>>>>> Stashed changes
     }
 
     public void sendVerifyEmail(EmailOtpRequestDto requestDto) {
@@ -43,7 +75,11 @@ public class AuthNotificationService {
         context.setVariable("otp", requestDto.getOtp());
         context.setVariable("name", requestDto.getName());
         String body = templateEngine.process("verify-email", context);
+<<<<<<< Updated upstream
         mailService.sendMail(requestDto.getEmail(), "Verify Your FitStudio Account ✅", body);
+=======
+        sendMail(requestDto.getEmail(), "Verify Your FitStudio Account ✅", body);
+>>>>>>> Stashed changes
     }
 
     public void SendCredentials(SendCredentialRequestDto requestDto) {
@@ -51,7 +87,11 @@ public class AuthNotificationService {
         context.setVariable("name", requestDto.getName());
         context.setVariable("password", requestDto.getPassword());
         String body = templateEngine.process("send-credentials", context);
+<<<<<<< Updated upstream
         mailService.sendMail(requestDto.getEmail(), "Your Fit Gym Account Credentials 🔑", body);
+=======
+        sendMail(requestDto.getEmail(), "Your Fit Gym Account Credentials 🔑", body);
+>>>>>>> Stashed changes
     }
 
     public void sendResetPasswordEmail(EmailOtpRequestDto requestDto) {
@@ -59,16 +99,52 @@ public class AuthNotificationService {
         context.setVariable("name", requestDto.getEmail());
         context.setVariable("otp", requestDto.getOtp());
         String body = templateEngine.process("reset-password", context);
+<<<<<<< Updated upstream
         mailService.sendMail(requestDto.getEmail(), "Reset Password", body);
+=======
+        sendMail(requestDto.getEmail(), "Reset Password", body);
+    }
+
+    public void sendMail(String to, String subject, String body) {
+        Email from = new Email(sender);
+        Email sendTo = new Email(to);
+        Content content = new Content("text/html", body);
+        Request request = new Request();
+        Mail mail = new Mail(from, subject, sendTo, content);
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sendGrid.api(request);
+            System.out.println("SendGrid Status: " + response.getStatusCode());
+        } catch (IOException e) {
+            throw new RuntimeException("error to process message");
+        }
+>>>>>>> Stashed changes
     }
 
     public void sendVerifyPhone(PhoneOtpRequestDto requestDto) {
         String message = "Hello " + requestDto.getName() +
                 " this is your FitStudio verification OTP " + requestDto.getOtp() +
                 "\n please verify your mobile no.";
+<<<<<<< Updated upstream
         smsService.sendSms(requestDto.getPhone(), message);
     }
 
+=======
+        sendSms(requestDto.getPhone(), message);
+    }
+
+    public void sendSms(String to, String sms) {
+        Message.creator(
+                new PhoneNumber(to),
+                new PhoneNumber(fromNumber),
+                sms
+        ).create();
+    }
+
+
+>>>>>>> Stashed changes
    /* public void sendVerifyPhone(PhoneOtpRequestDto requestDto) {
         String message = "Hello "+requestDto.getName()+
                 " this is your FitStudio verification OTP " + requestDto.getOtp() +
