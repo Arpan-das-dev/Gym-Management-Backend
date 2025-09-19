1c6d29e26f2feb2ff8ec94829985dabdac54fef8
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-19 15:21:28 +0530

    add session and trainer repostiories

--- memberService/src/main/java/com/gym/member_service/Repositories/TrainerRepository.java
+++ memberService/src/main/java/com/gym/member_service/Repositories/TrainerRepository.java
@@ -1,4 +1,19 @@
 package com.gym.member_service.Repositories;
 
-public interface TrainerRepository {
+import com.gym.member_service.Model.Trainer;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface TrainerRepository extends JpaRepository<Trainer,Long> {
+    /**
+     * Finds the trainer currently assigned to a given member.
+     *
+     * @param memberId ID of the member
+     * @return Optional containing Trainer if found, otherwise empty
+     */
+    @Query("SELECT t FROM Trainer t WHERE t.memberId = :memberId")
+    Optional<Trainer> findTrainerByMemberId(@Param("memberId") String memberId);
 }
