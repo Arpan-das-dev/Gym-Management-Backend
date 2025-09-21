021edc2da501dd783e2aa01fbdedee52aafc2028
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-21 22:11:31 +0530

    add dtos

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Responses/AllTrainerResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Responses/AllTrainerResponseDto.java
@@ -1,4 +1,24 @@
 package com.gym.trainerService.Dto.TrainerMangementDto.Responses;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllTrainerResponseDto {
+    private String id;
+    private String imageUrl;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String gender;
+    private double averageRating;
+    private LocalDateTime lastLoginTime;
 }
