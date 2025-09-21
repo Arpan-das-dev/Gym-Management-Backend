d0dad5fc8523017b6ea3dde9de2ab202b7af8af6
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-21 22:11:03 +0530

    add redis configs

--- trainerService/src/main/java/com/gym/trainerService/Configs/RedisConfig.java
+++ trainerService/src/main/java/com/gym/trainerService/Configs/RedisConfig.java
@@ -1,4 +1,62 @@
 package com.gym.trainerService.Configs;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.gym.trainerService.Dto.TrainerMangementDto.Responses.AllTrainerResponseDto;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
 public class RedisConfig {
+    @Bean
+    public ObjectMapper redisObjectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        om.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return om;
+    }
+
+    @Bean
+    public TypedJsonRedisSerializer<AllTrainerResponseDto> allTrainerResponseDtoRedisSerializer
+            (ObjectMapper redisObjectMapper){
+        return new TypedJsonRedisSerializer<>(redisObjectMapper, AllTrainerResponseDto.class);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory,
+                                   TypedJsonRedisSerializer<AllTrainerResponseDto> allTrainerResponseDtoRedisSerializer
+    )
+    {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .disableCachingNullValues();
+
+        Map<String, RedisCacheConfiguration> cacheConfigs = new HashMap<>();
+
+        cacheConfigs.put("AllTrainerCache",defaultConfig
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(allTrainerResponseDtoRedisSerializer))
+                .entryTtl(Duration.ofHours(6)));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigs)
+                .build();
+    }
 }
