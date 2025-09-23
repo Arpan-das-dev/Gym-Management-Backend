c830f0e73f776cf35d728dc3906b94cdfa3073c3
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-23 16:07:26 +0530

    add wrappers arround all trainers as list to avoid redis serialization/deserialization issue

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Wrappers/AllTrainerResponseDtoWrapper.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Wrappers/AllTrainerResponseDtoWrapper.java
@@ -1,4 +1,26 @@
 package com.gym.trainerService.Dto.TrainerMangementDto.Wrappers;
 
+import com.gym.trainerService.Dto.TrainerMangementDto.Responses.AllTrainerResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Wrapper DTO for a list of all trainer responses.
+ * <p>
+ * Encapsulates a list of {@link AllTrainerResponseDto} objects to avoid
+ * serialization/deserialization issues with caching systems such as Redis.
+ * </p>
+ *
+ * @see AllTrainerResponseDto
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllTrainerResponseDtoWrapper {
+    List<AllTrainerResponseDto> allTrainerResponseDtoWrappers;
 }
