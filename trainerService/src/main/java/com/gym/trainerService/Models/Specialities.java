cdf61910d9e12ace23b70deec7761ebed34d250a
Arpan-das-dev <dasarpan915@gmail.com>
2025-09-23 16:08:10 +0530

    add specialization model

--- trainerService/src/main/java/com/gym/trainerService/Models/Specialities.java
+++ trainerService/src/main/java/com/gym/trainerService/Models/Specialities.java
@@ -1,4 +1,28 @@
 package com.gym.trainerService.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "specialities",indexes = {
+        @Index(name = "idx_trainer_id",columnList = "trainerId"),
+        @Index(name = "idx_speciality",columnList = "speciality")
+})
 public class Specialities {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String trainerId;
+
+    @Column(nullable = false)
+    private String speciality;
 }
