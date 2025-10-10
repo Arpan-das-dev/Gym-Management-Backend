d6207e7677aec5f4d4a117952c9c38388a05fabf
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-10 12:23:16 +0530

    add memberDtos for member management service

--- trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Requests/AssignMemberRequestDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Requests/AssignMemberRequestDto.java
@@ -1,4 +1,33 @@
 package com.gym.trainerService.Dto.MemberDtos.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssignMemberRequestDto {
+    @NotBlank(message = "Member ID must not be blank")
+    private String memberId;
+
+    @NotBlank(message = "Trainer ID must not be blank")
+    private String trainerId;
+
+    @NotBlank(message = "MemberName can not be empty")
+    private String memberName;
+
+    @NotBlank(message = "Trainer profile image URL must be a valid URL")
+    private String trainerProfileImageUrl;
+
+    @NotNull(message = "Request date must not be null")
+    @PastOrPresent(message = "Request date cannot be in the future")
+    private LocalDate requestDate;
 }
--- trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Wrappers/AllMemberResponseWrapperDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Wrappers/AllMemberResponseWrapperDto.java
@@ -1,4 +1,16 @@
 package com.gym.trainerService.Dto.MemberDtos.Wrappers;
 
-public class AlllMemberResposeWrapperDto {
+import com.gym.trainerService.Dto.MemberDtos.Responses.MemberResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllMemberResponseWrapperDto {
+    List<MemberResponseDto> memberResponseDtoList;
 }
