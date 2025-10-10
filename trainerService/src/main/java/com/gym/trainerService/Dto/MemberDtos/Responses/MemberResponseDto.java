3d7b8724a4556946276dd04d73669d7d205e5a67
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-10 12:23:16 +0530

    add memberDtos for member management service

--- trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Responses/MemberResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Responses/MemberResponseDto.java
@@ -1,4 +1,20 @@
 package com.gym.trainerService.Dto.MemberDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberResponseDto {
+    private String memberId;
+    private String trainerId;
+    private String memberName;
+    private String memberProfileImageUrl;
+    private LocalDate eligibilityEnd;
 }
