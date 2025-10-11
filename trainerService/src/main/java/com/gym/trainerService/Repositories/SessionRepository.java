7dd4b420670ce767e99bb80b162ad60c6d19e526
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-11 19:13:13 +0530

    add session repository to interact with db using custom JPQL queries

--- trainerService/src/main/java/com/gym/trainerService/Repositories/SessionRepository.java
+++ trainerService/src/main/java/com/gym/trainerService/Repositories/SessionRepository.java
@@ -1,7 +1,33 @@
 package com.gym.trainerService.Repositories;
 
 import com.gym.trainerService.Models.Session;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface SessionRepository extends JpaRepository<Session,String > {
+
+    @Query("""
+       SELECT s FROM Session s
+         WHERE s.trainerId = :trainerId
+         AND s.sessionStartTime >= :currentTime
+         ORDER BY s.sessionStartTime ASC
+       """)
+    List<Session> findByTrainerId(@Param("trainerId") String trainerId,
+                                  @Param("now") LocalDateTime now);
+
+    @Query("SELECT s FROM Session s WHERE s.trainerId = :trainerId AND s.sessionStartTime < :currentTime ORDER BY s.sessionStartTime DESC")
+    Page<Session> findPaginatedDataByTrainerId(@Param("trainerId") String trainerId,
+                                               @Param("currentTime") LocalDateTime currentTime,
+                                               Pageable pageRequest);
+
+    @Query("SELECT s FROM Session s WHERE s.sessionStartTime >= :startTime AND s.sessionEndTime =< :endTime")
+    Optional<Session> sessionSlotCheck(@Param("startTIme") LocalDateTime startTime,
+                                       LocalDateTime endTime);
 }
