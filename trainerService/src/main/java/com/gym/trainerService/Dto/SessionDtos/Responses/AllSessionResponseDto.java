455098796b3988ede2fb58313dccda6cfaecca2f
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-11 19:06:31 +0530

    add response dto as return response

--- trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/AllSessionResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/AllSessionResponseDto.java
@@ -1,4 +1,19 @@
 package com.gym.trainerService.Dto.SessionDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllSessionResponseDto {
+    private String sessionId;
+    private String memberId;
+    private String sessionName;
+    private LocalDateTime sessionStartTime;
+    private LocalDateTime sessionEndTime;
 }
