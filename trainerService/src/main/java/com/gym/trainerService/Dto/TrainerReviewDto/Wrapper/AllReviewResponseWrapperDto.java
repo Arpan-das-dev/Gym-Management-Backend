1cc98e81540db129cf38d4983ba2ecc30d39d817
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-11 19:09:45 +0530

    add wrappers to avoid redis serialization issue

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Wrapper/AllReviewResponseWrapperDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Wrapper/AllReviewResponseWrapperDto.java
@@ -1,4 +1,17 @@
 package com.gym.trainerService.Dto.TrainerReviewDto.Wrapper;
 
-public class AllReviewResponseDto {
+import com.gym.trainerService.Dto.TrainerReviewDto.Responses.ReviewResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllReviewResponseWrapperDto {
+    List<ReviewResponseDto> reviewResponseDtoList;
 }
