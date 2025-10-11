0085a63f03e7a1c8d6b5a503607f5f1a02a4ba27
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-11 19:14:35 +0530

    add reponse dto to get a view of all reviews for specific trainers

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Responses/ReviewResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Responses/ReviewResponseDto.java
@@ -1,4 +1,24 @@
-package com.gym.trainerService.Dto.TrainerReviewDto.Requests;
+package com.gym.trainerService.Dto.TrainerReviewDto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewResponseDto {
+    private String reviewId;
+    private String userId;
+    private String userName;
+    private String userRole;
+    private LocalDateTime reviewDate;
+    private int helpFullVote;
+    private int notHelpFullVote;
+    private String comment;
+    private Double review;
 }
