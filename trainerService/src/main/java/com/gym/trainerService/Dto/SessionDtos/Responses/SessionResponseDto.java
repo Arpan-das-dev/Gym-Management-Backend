fb56aeda021329b679856843f5f6c1bf9c813d47
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-11 19:07:47 +0530

    add reponse dtos to send to member microservice for add and update sessions

--- trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/SessionResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/SessionResponseDto.java
@@ -1,4 +1,18 @@
 package com.gym.trainerService.Dto.SessionDtos.Responses;
 
-public class SessionRepsonseDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SessionResponseDto {
+    private String sessionId;
+    private String sessionName;
+    private LocalDateTime sessionDate;
+    private Double duration;
 }
