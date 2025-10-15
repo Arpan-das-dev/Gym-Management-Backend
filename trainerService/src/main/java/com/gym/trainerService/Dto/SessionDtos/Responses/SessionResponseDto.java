206108bb38e4be241133b6473548dc667eae068c
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-15 07:32:36 +0530

    add dto to send member service to add session

--- trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/SessionResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/SessionResponseDto.java
@@ -6,13 +6,27 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+/**
+ * DTO representing the session information sent to another service
+ * (for example, Member Service) after a new session is created.
+ * @author Arpan Das
+ * @since 1.0
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class SessionResponseDto {
+
+    /** Unique identifier of the session. */
     private String sessionId;
+
+    /** Name/title of the session. */
     private String sessionName;
+
+    /** Date/time of the session. */
     private LocalDateTime sessionDate;
+
+    /** Duration of the session in hours. */
     private Double duration;
 }
