8fa3c2eb83949fb36cd525595f9323b2513efaa2
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-15 07:33:10 +0530

    add wrapper to avoid redis serialziation issue

--- trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Wrappers/AllSessionsWrapperDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Wrappers/AllSessionsWrapperDto.java
@@ -8,10 +8,21 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * Wrapper DTO containing a list of session response objects.
+ * <p>
+ * Commonly used to send a paginated or grouped list of sessions
+ * for a given trainer.
+ * </p>
+ * @author Arpan
+ * @since 1.0
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AllSessionsWrapperDto {
+
+    /** Collection of session details. */
     List<AllSessionResponseDto> responseDtoList;
 }
