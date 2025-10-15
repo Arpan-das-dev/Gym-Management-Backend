f5e0dfff52d4501ececb2c2d6216aa74fd3ff769
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-15 07:31:36 +0530

    add dto to send member service to update session

--- trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/UpdateSessionResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/SessionDtos/Responses/UpdateSessionResponseDto.java
@@ -7,13 +7,29 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * DTO representing the response after updating an existing session.
+ * <p>
+ * Used to confirm the updated session details returned to the client.
+ * </p>
+ * @author Arpan Das
+ * @since 1.0
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UpdateSessionResponseDto {
+
+    /** ID of the trainer associated with the session. */
     private String trainerId;
+
+    /** Name/title of the updated session. */
     private String sessionName;
+
+    /** Updated session start time. */
     private LocalDateTime sessionStartTime;
+
+    /** Updated session end time. */
     private LocalDateTime sessionEndTime;
 }
