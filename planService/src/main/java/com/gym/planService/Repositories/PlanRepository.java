66995b1a4db858159f1708897503d70c25e2e94a
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-16 15:32:59 +0530

    add repostiory to interact plans in database

--- planService/src/main/java/com/gym/planService/Repositories/PlanRepository.java
+++ planService/src/main/java/com/gym/planService/Repositories/PlanRepository.java
@@ -1,4 +1,18 @@
 package com.gym.planService.Repositories;
 
-public interface PlanRepository {
+import com.gym.planService.Models.Plan;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PlanRepository extends JpaRepository< Plan, String > {
+
+
+    boolean existsByPlanName(String planName);
+
+    @Modifying
+    @Query("DELETE FROM Plan p WHERE p.id = :id")
+    int deletedById(@Param("id") String id);
+
 }
