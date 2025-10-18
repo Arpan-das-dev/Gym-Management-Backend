c9f96007e09f60b81bf8358ff46ff70b820b9c9b
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:16:43 +0530

    add repository to interact with db

--- planService/src/main/java/com/gym/planService/Repositories/PlanCuponCodeRepository.java
+++ planService/src/main/java/com/gym/planService/Repositories/PlanCuponCodeRepository.java
@@ -1,7 +1,13 @@
 package com.gym.planService.Repositories;
 
 import com.gym.planService.Models.PlanCuponCode;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PlanCuponCodeRepository extends JpaRepository<PlanCuponCode,String> {
+    @Query("SELECT c FROM planservice.PlanCuponCode c where planId = :planId")
+    List<PlanCuponCode> findAllByPlanId(@Param("planId") String planId);
 }
