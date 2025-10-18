69ca4f6eb564a4c8b24e152ac544edc3c5df5aec
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:13:40 +0530

    add dto for response

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanResponseDto.java
@@ -1,4 +1,21 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanResponseDto {
+
+    private String planId;
+    private String planName;
+    private Double price;
+    private Integer duration;
+    private List<String> planFeatures;
 }
