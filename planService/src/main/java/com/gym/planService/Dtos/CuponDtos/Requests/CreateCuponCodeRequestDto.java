0285f1b9b4a7c5c3e4a3a153dc42b497624dc682
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:11:18 +0530

    add dtos for requests to manage plan cupon codes

--- planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Requests/CreateCuponCodeRequestDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Requests/CreateCuponCodeRequestDto.java
@@ -1,4 +1,19 @@
 package com.gym.planService.Dtos.CuponDtos.Requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateCuponCodeRequestDto {
+
+    private String cuponCode;
+    private LocalDate validity;
+    private Double offPercentage;
 }
--- planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Requests/UpdateCuponRequestDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Requests/UpdateCuponRequestDto.java
@@ -1,4 +1,19 @@
 package com.gym.planService.Dtos.CuponDtos.Requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UpdateCuponRequestDto {
+
+    private String planId;
+    private LocalDate validity;
+    private Double offPercentage;
 }
