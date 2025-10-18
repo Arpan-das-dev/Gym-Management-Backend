2aacbca1b897764a555e0e5bc63838a2ba48ee94
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:15:12 +0530

    update handler accronding to new custom exceptions

--- planService/src/main/java/com/gym/planService/Exception/Handler/GlobalExceptionHandler.java
+++ planService/src/main/java/com/gym/planService/Exception/Handler/GlobalExceptionHandler.java
@@ -1,4 +1,38 @@
 package com.gym.planService.Exception.Handler;
 
+import com.gym.planService.Exception.Custom.CuponCodeNotFoundException;
+import com.gym.planService.Exception.Custom.DuplicateCuponCodeFoundException;
+import com.gym.planService.Exception.Custom.DuplicatePlanFoundException;
+import com.gym.planService.Exception.Custom.PlanNotFoundException;
+import com.gym.planService.Exception.Model.ErrorResponse;
+import com.gym.planService.Exception.Utils.ExceptionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({
+            PlanNotFoundException.class,
+            CuponCodeNotFoundException.class
+    })
+    public ResponseEntity<ErrorResponse> handleNotFound(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler({
+            DuplicatePlanFoundException.class,
+            DuplicateCuponCodeFoundException.class
+    })
+    public ResponseEntity<ErrorResponse> handleConflict(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobal(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request);
+    }
 }
