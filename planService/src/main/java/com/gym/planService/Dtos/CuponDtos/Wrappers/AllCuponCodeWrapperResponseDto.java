314c25997cf723ad60db65a9d01222d8e31b3ac3
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:12:27 +0530

    add wrapper to avoid redis serialzation issue

--- planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Wrappers/AllCuponCodeWrapperResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Wrappers/AllCuponCodeWrapperResponseDto.java
@@ -1,4 +1,17 @@
 package com.gym.planService.Dtos.CuponDtos.Wrappers;
 
-public class CuponCodeWrapperResponseDto {
+import com.gym.planService.Dtos.CuponDtos.Responses.CuponCodeResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllCuponCodeWrapperResponseDto {
+    List<CuponCodeResponseDto>responseDtoList;
 }
