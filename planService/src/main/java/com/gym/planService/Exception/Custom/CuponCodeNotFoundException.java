c26ba6fa7fe8477874ad8fc2afb0b64fd6ac4fc6
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:14:34 +0530

    add custom exceptions

--- planService/src/main/java/com/gym/planService/Exception/Custom/CuponCodeNotFoundException.java
+++ planService/src/main/java/com/gym/planService/Exception/Custom/CuponCodeNotFoundException.java
@@ -1,7 +1,7 @@
 package com.gym.planService.Exception.Custom;
 
 public class CuponCodeNotFoundException extends RuntimeException {
-  public CuponCodeNotFoundException(String message) {
-    super(message);
-  }
+    public CuponCodeNotFoundException(String message) {
+        super(message);
+    }
 }
