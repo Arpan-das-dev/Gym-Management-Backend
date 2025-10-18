13d704c189103d878b310a2434741648d142cd25
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-18 17:12:02 +0530

    add dto for response

--- planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Responses/CuponCodeResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Responses/CuponCodeResponseDto.java
@@ -1,4 +1,18 @@
 package com.gym.planService.Dtos.CuponDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CuponCodeResponseDto {
+    private String cuponCode;
+    private LocalDate validityDate;
+    private Double offPercentage;
 }
