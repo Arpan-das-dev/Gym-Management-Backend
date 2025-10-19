141cded7e8c5b833c753ab8c242247cd9e9dd653
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 22:05:05 +0530

    add controller for plan payement feature

--- planService/src/main/java/com/gym/planService/Controllers/PaymentController.java
+++ planService/src/main/java/com/gym/planService/Controllers/PaymentController.java
@@ -1,4 +1,32 @@
 package com.gym.planService.Controllers;
 
+import com.gym.planService.Dtos.OrderDtos.Requests.PlanPaymentRequestDto;
+
+import com.gym.planService.Services.PaymentService.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("${payment-service.Base_Url.Pay}")
+@RequiredArgsConstructor
 public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PostMapping("/buyPlan")
+    public ResponseEntity<String> makePayment(@Valid @RequestBody PlanPaymentRequestDto requestDto) {
+        log.info("");
+        String response = paymentService.buyPlan(requestDto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+    }
 }
