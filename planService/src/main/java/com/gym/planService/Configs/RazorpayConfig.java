f1c3f064b50bc01ae739e8b187d7a03461b11503
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 22:01:43 +0530

    add configuration for razorpay

--- planService/src/main/java/com/gym/planService/Configs/RazorpayConfig.java
+++ planService/src/main/java/com/gym/planService/Configs/RazorpayConfig.java
@@ -1,4 +1,26 @@
 package com.gym.planService.Configs;
 
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RazorpayConfig {
+
+    private final String razorpay_KEY;
+    private final String razorpay_SECRET;
+
+    public RazorpayConfig(
+            @Value("${razorpay.api.key}") String razorpay_KEY,
+            @Value("${razorpay.api.secret}") String razorpay_SECRET) {
+        this.razorpay_KEY = razorpay_KEY;
+        this.razorpay_SECRET = razorpay_SECRET;
+    }
+
+    @Bean
+    public RazorpayClient razorpayClient() throws RazorpayException {
+        return new RazorpayClient(razorpay_KEY,razorpay_SECRET);
+    }
 }
