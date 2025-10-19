af4693a0924f1c51eeba95e2324187c49dcd7a19
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 22:11:05 +0530

    add util component to generate custom orderId for payment

--- planService/src/main/java/com/gym/planService/Utils/PaymentIdGenUtil.java
+++ planService/src/main/java/com/gym/planService/Utils/PaymentIdGenUtil.java
@@ -1,4 +1,8 @@
 package com.gym.planService.Utils;
 
+import java.time.LocalDate;
+
 public class PaymentIdGenUtil {
+    public String generatePaymentId(String userId, String planId, LocalDate paymentDate) {
+    }
 }
