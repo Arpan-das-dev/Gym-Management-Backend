6db31c07dab21faddd5eb11f61d4884d68be74ca
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 22:06:24 +0530

    add request dto for making payment

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Requests/PlanPaymentRequestDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Requests/PlanPaymentRequestDto.java
@@ -1,4 +1,21 @@
 package com.gym.planService.Dtos.OrderDtos.Requests;
 
-public class PaymentRequestDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlanPaymentRequestDto {
+
+    private String userId;
+    private String planId;
+    private String currency;
+    private Double amount;
+    private String cuponCode;
+    private LocalDate paymentDate;
 }
