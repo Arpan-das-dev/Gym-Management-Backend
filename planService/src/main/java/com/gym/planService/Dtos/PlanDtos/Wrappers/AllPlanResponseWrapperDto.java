2a212791d78a4d5f1f96334b1acc380190f0d3ec
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 12:20:21 +0530

    add wrapper dto to avoid redis serialization/deserialization issue

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Wrappers/AllPlanResponseWrapperDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Wrappers/AllPlanResponseWrapperDto.java
@@ -8,10 +8,50 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * Wrapper Data Transfer Object (DTO) encapsulating a list of plan response DTOs.
+ * <p>
+ * This class is used to standardize the API response format when returning multiple subscription plans.
+ * It packages a collection of {@link PlanResponseDto} objects within a single container.
+ * </p>
+ *
+ * <p>Example JSON representation:</p>
+ * <pre>
+ * {
+ *   "responseDtoList": [
+ *     {
+ *       "planId": "BASIC_2026",
+ *       "planName": "Basic Plan",
+ *       "price": 2999.0,
+ *       "duration": 365,
+ *       "planFeatures": ["Feature 1", "Feature 2"]
+ *     },
+ *     {
+ *       "planId": "PREMIUM_2026",
+ *       "planName": "Premium Plan",
+ *       "price": 5999.0,
+ *       "duration": 365,
+ *       "planFeatures": ["Feature A", "Feature B", "Feature C"]
+ *     }
+ *   ]
+ * }
+ * </pre>
+ *
+ * <p>This wrapper facilitates handling lists of plans smoothly in REST API communications.</p>
+ *
+ * @author : Arpan Das
+ * @version : 1.0
+ * @since : 2025-10-19
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AllPlanResponseWrapperDto {
-    List<PlanResponseDto> responseDtoList;
+
+    /**
+     * List of plan response DTOs.
+     * This list contains individual plan details wrapped in {@link PlanResponseDto}.
+     */
+    private List<PlanResponseDto> responseDtoList;
 }
