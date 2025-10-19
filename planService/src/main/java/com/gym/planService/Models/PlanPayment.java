400c605c7564518b93c30b3f57356a9b0193d7ee
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-19 22:08:37 +0530

    add model for plan payment

--- planService/src/main/java/com/gym/planService/Models/PlanPayment.java
+++ planService/src/main/java/com/gym/planService/Models/PlanPayment.java
@@ -1,4 +1,28 @@
 package com.gym.planService.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class PlanPayment {
+
+    @Id
+    private String paymentId;
+    private Double paidPrice;
+    private String currency;
+    private LocalDate paymentDate;
+    private String paymentMonth;
+    private Integer paymentYear;
 }
