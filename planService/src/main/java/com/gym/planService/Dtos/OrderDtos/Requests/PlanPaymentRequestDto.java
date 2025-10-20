92abd0574e1f507d83bdb185f51cfef326405435
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-20 11:03:14 +0530

    update the plan payment dto

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Requests/PlanPaymentRequestDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Requests/PlanPaymentRequestDto.java
@@ -5,17 +5,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlanPaymentRequestDto {
 
+    private String userName;
     private String userId;
     private String planId;
     private String currency;
     private Double amount;
     private String cuponCode;
-    private LocalDate paymentDate;
+    private LocalDateTime paymentDate;
 }
