1b2c1d5e332f9c4a74b4331747102ba4fedb08eb
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-22 12:28:21 +0530

    add webclient to interact with other services

--- planService/src/main/java/com/gym/planService/Configs/WebClientConfig.java
+++ planService/src/main/java/com/gym/planService/Configs/WebClientConfig.java
@@ -1,4 +1,14 @@
 package com.gym.planService.Configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class WebClientConfig {
+
+    @Bean
+    public WebClient.Builder webClient(){
+        return WebClient.builder();
+    }
 }
