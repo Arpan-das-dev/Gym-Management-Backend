5361afc9e65a4090e51dbdd51edcb764a115da01
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-22 12:33:19 +0530

    add response dto to send notification service with attachment

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/PlanNotificationResponse.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/PlanNotificationResponse.java
@@ -1,4 +1,18 @@
 package com.gym.planService.Dtos.OrderDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanNotificationResponse {
+
+    private String userName;
+    private String planName;
+    private Integer planDuration;
+    private Double planPrice;
 }
