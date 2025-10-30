1f9d61158eb5dc6146dd271ec5ca54970031cd42
Arpan-das-dev <dasarpan915@gmail.com>
2025-10-30 20:42:36 +0530

    add redis configuration for caching

--- authService/src/main/java/com/gym/authservice/Config/Configuraion/RedisConfig.java
+++ authService/src/main/java/com/gym/authservice/Config/Configuraion/RedisConfig.java
@@ -1,14 +1,40 @@
 package com.gym.authservice.Config.Configuraion;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.data.redis.cache.RedisCacheManager.builder;
 
 @Configuration
 public class RedisConfig {
     @Bean
     public StringRedisTemplate redisTemplate(RedisConnectionFactory connectionFactory){
         return new StringRedisTemplate(connectionFactory);
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new StringRedisSerializer()))
+                .disableCachingNullValues();
+
+        Map<String , RedisCacheConfiguration> configurationMap = new HashMap<>();
+       configurationMap.put("userInfo",defaultConfig.entryTtl(Duration.ofHours(6)));
+       return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(configurationMap)
+                .build();
     }
 }
