7b0cb816b4bcd5eed9d97cc3ae8e9b8b476901bd
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-03 20:54:50 +0530

    updates reponses

--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/UserCreationResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/UserCreationResponseDto.java
@@ -1,4 +1,12 @@
 package com.gym.adminservice.Dto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserCreationResponseDto {
+    private String message;
 }
