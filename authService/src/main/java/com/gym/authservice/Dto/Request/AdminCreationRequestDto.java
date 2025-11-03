47b47f7b21dac896dd13347a5609b465fdb013fd
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-03 20:52:57 +0530

    add request dto to create only admin

--- authService/src/main/java/com/gym/authservice/Dto/Request/AdminCreationRequestDto.java
+++ authService/src/main/java/com/gym/authservice/Dto/Request/AdminCreationRequestDto.java
@@ -1,4 +1,28 @@
 package com.gym.authservice.Dto.Request;
 
+import com.gym.authservice.Roles.RoleType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminCreationRequestDto {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private boolean isEmailVerified;
+    private boolean isPhoneVerified;
+    private boolean isApproved;
+    private LocalDate joinDate;
+    private String gender;
+    private RoleType role;
 }
