b8551f745d6bbc5ca9e8967c5fac0eebab71a3ff
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-04 17:24:43 +0530

    add controller and dto

--- notificationService/src/main/java/com/gym/notificationservice/Controller/PaymentNotificationController.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/PaymentNotificationController.java
@@ -1,4 +1,26 @@
 package com.gym.notificationservice.Controller;
 
+import com.gym.notificationservice.Dto.PaymentNotificationDtos.Requests.PlanReceiptRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@RestController
+@RequestMapping("${plan-paymentService.url}")
+@RequiredArgsConstructor
 public class PaymentNotificationController {
+
+    @PostMapping(value = "/buyPlan", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void sendPaymentReceipt(
+            @RequestPart("attachment") MultipartFile attachment,
+            @RequestPart("response") PlanReceiptRequestDto requestDto) {
+
+        log.info("Received PDF receipt ({} bytes) for user {}", attachment.getSize(), requestDto.getUserName());
+    }
 }
--- notificationService/src/main/java/com/gym/notificationservice/Dto/PaymentNotificationDtos/Requests/PlanReceiptRequestDto.java
+++ notificationService/src/main/java/com/gym/notificationservice/Dto/PaymentNotificationDtos/Requests/PlanReceiptRequestDto.java
@@ -1,4 +1,18 @@
 package com.gym.notificationservice.Dto.PaymentNotificationDtos.Requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanReceiptRequestDto {
+    private String userName;
+    private String userMail;
+    private String planName;
+    private Integer planDuration;
+    private Double planPrice;
 }
