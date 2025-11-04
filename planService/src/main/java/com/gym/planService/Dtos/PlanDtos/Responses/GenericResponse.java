ec66e1be825a8651af8aaf378d5c0d58db8fc3f9
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-04 17:27:43 +0530

    add generic response to return value in string

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/GenericResponse.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/GenericResponse.java
@@ -1,4 +1,12 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GenericResponse {
+    String message;
 }
