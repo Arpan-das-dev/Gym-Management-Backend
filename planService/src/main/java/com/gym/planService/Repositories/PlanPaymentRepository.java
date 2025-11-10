7f4a543867363042b15b84e2a38ddab7d903a133
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-10 21:32:25 +0530

    add new methods with custom jpql queries

--- planService/src/main/java/com/gym/planService/Repositories/PlanPaymentRepository.java
+++ planService/src/main/java/com/gym/planService/Repositories/PlanPaymentRepository.java
@@ -1,7 +1,25 @@
 package com.gym.planService.Repositories;
 
 import com.gym.planService.Models.PlanPayment;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface PlanPaymentRepository extends JpaRepository<PlanPayment,String> {
+
+    @Query("SELECT p FROM PlanPayment p WHERE p.orderId = :orderId")
+    Optional<PlanPayment> findByOrderId(@Param("orderId") String orderId);
+
+    @Query("SELECT SUM(p.paidPrice) FROM PlanPayment p " +
+            "WHERE p.paymentStatus = 'SUCCESS' AND p.paymentMonth = :month AND p.paymentYear = :year")
+    Optional<Double> sumRevenueByMonthAndYear(@Param("month") String month,
+                                              @Param("year") Integer year);
+
+    @Query("SELECT p FROM PlanPayment p " +
+            "WHERE p.paymentStatus = 'SUCCESS' AND p.paymentMonth =:month AND p.paymentYear = :year")
+    List<PlanPayment> findByCurrentMonthAndYear(@Param("month") String month,
+                                                @Param("year") Integer year);
 }
