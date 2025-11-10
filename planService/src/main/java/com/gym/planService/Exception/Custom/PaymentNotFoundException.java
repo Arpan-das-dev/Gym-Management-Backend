e134569104a023213123855ecb94090b61aa0132
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-10 21:15:40 +0530

    add new custom exceptions

--- planService/src/main/java/com/gym/planService/Exception/Custom/PaymentNotFoundException.java
+++ planService/src/main/java/com/gym/planService/Exception/Custom/PaymentNotFoundException.java
@@ -1,7 +1,7 @@
 package com.gym.planService.Exception.Custom;
 
 public class PaymentNotFoundException extends RuntimeException {
-  public PaymentNotFoundException(String message) {
-    super(message);
-  }
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
 }
