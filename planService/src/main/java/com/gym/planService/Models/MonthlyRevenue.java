da7d8559552216cfd4ffb9f3d8674dba76e5e74e
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-10 21:31:43 +0530

    add new model to store revenew for each month

--- planService/src/main/java/com/gym/planService/Models/MonthlyRevenue.java
+++ planService/src/main/java/com/gym/planService/Models/MonthlyRevenue.java
@@ -1,4 +1,34 @@
 package com.gym.planService.Models;
 
-public class MonthlyReveniew {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class MonthlyRevenue {
+
+    @Id
+    private LocalDate month;
+
+    @Column(name = "revenue")
+    private Double monthlyRevenue;
+
+    @Column(name = "receipt_url")
+    private String receiptUrl;
+
+    private String currentMonth;
+
+    private Integer currentYear;
 }
