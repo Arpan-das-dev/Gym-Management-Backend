a00940be5a78417725febb028f4ca5f2c5e299cf
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-10 21:17:23 +0530

    add new response dtos and it's wrapper to send response as well as caching the data

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/MonthlyReviewResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/MonthlyReviewResponseDto.java
@@ -1,4 +1,16 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonthlyReviewResponseDto {
+    private String month;
+    private Double revenue;
+    private Double change;
 }
