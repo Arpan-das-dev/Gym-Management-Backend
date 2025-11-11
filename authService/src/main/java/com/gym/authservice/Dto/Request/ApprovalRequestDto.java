3964490801353ec4079bb910cc6425851f1e9bbf
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-11 21:44:27 +0530

    add approval request to approve user's request

--- authService/src/main/java/com/gym/authservice/Dto/Request/ApprovalRequestDto.java
+++ authService/src/main/java/com/gym/authservice/Dto/Request/ApprovalRequestDto.java
@@ -1,4 +1,16 @@
 package com.gym.authservice.Dto.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApprovalRequestDto {
+
+    private String email;
+    private boolean approval;
 }
