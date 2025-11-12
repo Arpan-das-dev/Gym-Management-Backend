fb14d7f64ca104ba6ca22bf29a765f56592da7f8
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-13 03:34:18 +0530

    add exception handler, model, error response building utility

--- adminService/src/main/java/com/gym/adminservice/Exceptions/Handler/GlobalExceptionHandler.java
+++ adminService/src/main/java/com/gym/adminservice/Exceptions/Handler/GlobalExceptionHandler.java
@@ -1,4 +1,32 @@
 package com.gym.adminservice.Exceptions.Handler;
 
+import com.gym.adminservice.Exceptions.Custom.PlanNotFounException;
+import com.gym.adminservice.Exceptions.Custom.RequestNotFoundException;
+import com.gym.adminservice.Exceptions.Model.ErrorResponse;
+import com.gym.adminservice.Exceptions.Utils.ExceptionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.concurrent.ExecutionException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({RequestNotFoundException.class,
+    PlanNotFounException.class})
+    ResponseEntity<ErrorResponse> handleNotFound(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request);
+    }
+
+    @ExceptionHandler({ExecutionException.class, InterruptedException.class})
+    ResponseEntity<ErrorResponse> handleWebClient(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage(), request);
+    }
+    @ExceptionHandler({Exception.class})
+    ResponseEntity<ErrorResponse> handleGlobal(Exception ex, HttpServletRequest request) {
+        return ExceptionUtil.buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,ex.getMessage(),request);
+    }
 }
--- adminService/src/main/java/com/gym/adminservice/Exceptions/Model/ErrorResponse.java
+++ adminService/src/main/java/com/gym/adminservice/Exceptions/Model/ErrorResponse.java
@@ -1,4 +1,40 @@
 package com.gym.adminservice.Exceptions.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class ErrorResponse {
+    /**
+     * The exact time when the error occurred, represented in ISO‑8601 format.
+     */
+    private LocalDateTime timestamp;
+
+    /**
+     * The HTTP status code representing the type of error (e.g., 404, 500).
+     */
+    private int status;
+
+    /**
+     * The short error reason phrase or category (e.g., "Bad Request", "Internal Server Error").
+     */
+    private String error;
+
+    /**
+     * A more detailed explanation or custom message describing the cause of the error.
+     */
+    private String message;
+
+    /**
+     * The exact path or endpoint where the error originated, useful for debugging.
+     */
+    private String path;
 }
