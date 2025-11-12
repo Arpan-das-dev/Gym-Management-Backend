8a971a51797e75b4e8eaec7b6ecdcbf4bce2e7f0
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-13 03:36:15 +0530

    use completable return type and changes return type as required

--- adminService/src/main/java/com/gym/adminservice/Services/PlanServices/MembershipPlanManagementService.java
+++ adminService/src/main/java/com/gym/adminservice/Services/PlanServices/MembershipPlanManagementService.java
@@ -3,17 +3,18 @@ package com.gym.adminservice.Services.PlanServices;
 import com.gym.adminservice.Dto.PlanDtos.Requests.PlanCreateRequestDto;
 import com.gym.adminservice.Dto.PlanDtos.Requests.UpdatePlanRequestDto;
 import com.gym.adminservice.Dto.PlanDtos.Responses.CreationResponseDto;
-import com.gym.adminservice.Dto.PlanDtos.Responses.UpdateResponseDto;
+import com.gym.adminservice.Exceptions.Custom.PlanNotFounException;
 import com.gym.adminservice.Services.WebClientServices.WebClientPlanService;
 import com.gym.adminservice.Utils.PlanIdGenUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 
@@ -22,8 +23,8 @@ public class MembershipPlanManagementService {
     private final PlanIdGenUtil planIdGenUtil;
     private final WebClientPlanService webClientPlanService;
 
-    @CacheEvict(value = "PlaneCache",allEntries = true)
-    public CreationResponseDto createPlan(PlanCreateRequestDto requestDto) {
+
+    public String createPlan(PlanCreateRequestDto requestDto) throws ExecutionException, InterruptedException {
         String customId = planIdGenUtil.generatePlanId(
                 requestDto.getPlanName(), requestDto.getDuration(), requestDto.getPrice()
         );
@@ -35,32 +36,28 @@ public class MembershipPlanManagementService {
                 .features(requestDto.getFeatures())
                 .build();
 
-        webClientPlanService.sendCreationToPlanService(responseDto);
-        return responseDto;
+        String response =  webClientPlanService.sendCreationToPlanService(responseDto).get();
+        log.info("response received from plan service to create plan ::{}",response);
+        return response;
     }
 
-    @Cacheable(value = "PlaneCache")
+
     public List<CreationResponseDto> getAllPlans() {
         return webClientPlanService.getAllPlansFromPlanService();
     }
 
-    @CacheEvict(value = "PlaneCache",allEntries = true)
-    public UpdateResponseDto updatePlan(String id, UpdatePlanRequestDto requestDto) {
-        UpdateResponseDto responseDto = UpdateResponseDto.builder()
-                .id(requestDto.getId())
-                .planName(requestDto.getPlanName())
-                .price(requestDto.getPrice())
-                .duration(requestDto.getDuration())
-                .features(requestDto.getFeatures())
-                .build();
 
-        webClientPlanService.sendUpdateCreationToPlanService(id,requestDto);
-        return responseDto;
+    public String  updatePlan(String id, UpdatePlanRequestDto requestDto) throws ExecutionException, InterruptedException {
+        String response =  webClientPlanService.sendUpdateCreationToPlanService(id,requestDto).get();
+        log.info("response received from plan service to update plan ::{}",response);
+        return response;
     }
 
-    @CacheEvict(value = "PlaneCache",allEntries = true)
-    public void deletePlan(String id) {
-        webClientPlanService.sendDeletionRequestById(id);
+
+    public String  deletePlan(String id) throws ExecutionException, InterruptedException, PlanNotFounException {
+        String response =  webClientPlanService.sendDeletionRequestById(id).get();
+        log.info("response received from plan service to delete plan ::{}",response);
+        return response;
     }
 
 }
