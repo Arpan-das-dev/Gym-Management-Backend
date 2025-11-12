6a72dc8877779a6ffcc1f4fa74de6a099d246ae7
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-13 03:34:18 +0530

    add exception handler, model, error response building utility

--- adminService/src/main/java/com/gym/adminservice/Exceptions/Utils/ExceptionUtil.java
+++ adminService/src/main/java/com/gym/adminservice/Exceptions/Utils/ExceptionUtil.java
@@ -1,4 +1,26 @@
 package com.gym.adminservice.Exceptions.Utils;
 
+import com.gym.adminservice.Exceptions.Model.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
 public class ExceptionUtil {
+    private ExceptionUtil() {
+    }
+
+    public static ResponseEntity<ErrorResponse> buildErrorResponse(
+            HttpStatus status, String message, HttpServletRequest request) {
+
+        return ResponseEntity.status(status)
+                .body(new ErrorResponse(
+                        LocalDateTime.now(),
+                        status.value(),
+                        status.getReasonPhrase(),
+                        message,
+                        request.getRequestURI()
+                ));
+    }
 }
