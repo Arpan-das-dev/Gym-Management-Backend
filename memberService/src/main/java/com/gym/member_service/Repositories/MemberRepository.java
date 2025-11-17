c2cad03ce36073a797e708d4bce7e519f5e1772b
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-17 21:39:06 +0530

    add new method with custom jpql query

--- memberService/src/main/java/com/gym/member_service/Repositories/MemberRepository.java
+++ memberService/src/main/java/com/gym/member_service/Repositories/MemberRepository.java
@@ -14,49 +14,53 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 
-        @EntityGraph(attributePaths = { "weightBmiEntries", "prProgresses", "dailyRoutines.exercises" })
-        Optional<Member> findWithDetailsById(String id);
+    @EntityGraph(attributePaths = {"weightBmiEntries", "prProgresses", "dailyRoutines.exercises"})
+    Optional<Member> findWithDetailsById(String id);
 
-        // decrease the duration of members plan left duration by 1 if the current
-        // duration is less than -10
-        @Modifying
-        @Transactional
-        @Query(value = "UPDATE members  SET plan_duration_left = plan_duration_left-1 " +
-                        "WHERE plan_duration_left>= -10", nativeQuery = true)
-        int decrementDurationForAllMembers();
+    // decrease the duration of members plan left duration by 1 if the current
+    // duration is less than -10
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE members  SET plan_duration_left = plan_duration_left-1 " +
+            "WHERE plan_duration_left>= -10", nativeQuery = true)
+    int decrementDurationForAllMembers();
 
-        // when the plan expires it will set the all details about plan as null
-        @Modifying
-        @Transactional
-        @Query(value = "UPDATE members  SET plan_name = null, planid = null, active_plan = false " +
-                        "WHERE plan_expiration < :now", nativeQuery = true)
-        int expirePlan(@Param("now") LocalDateTime now);
+    // when the plan expires it will set the all details about plan as null
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE members  SET plan_name = null, planid = null, active_plan = false " +
+            "WHERE plan_expiration < :now", nativeQuery = true)
+    int expirePlan(@Param("now") LocalDateTime now);
 
-        // frozen the account which plan duration is less than -10
-        @Modifying
-        @Transactional
-        @Query(value = "UPDATE members  SET frozen = true " +
-                        "WHERE plan_duration_left <=- 10", nativeQuery = true)
-        int freezeExpiredAccounts();
+    // frozen the account which plan duration is less than -10
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE members  SET frozen = true " +
+            "WHERE plan_duration_left <=- 10", nativeQuery = true)
+    int freezeExpiredAccounts();
 
-        // select all those members whose account is frozen
-        @Query(value = "SELECT * FROM members " +
-                        "WHERE frozen = true", nativeQuery = true)
-        List<Member> getFrozenMemberList();
+    // select all those members whose account is frozen
+    @Query(value = "SELECT * FROM members " +
+            "WHERE frozen = true", nativeQuery = true)
+    List<Member> getFrozenMemberList();
 
-        // returns an integer value of members with specific plan name
-        @Query(value = "SELECT count(*) FROM members " +
-                        "WHERE plan_name = :name", nativeQuery = true)
-        int memberCountWithPlansNameOf(@Param("name") String planName);
+    // returns an integer value of members with specific plan name
+    @Query(value = "SELECT count(*) FROM members " +
+            "WHERE plan_name = :name", nativeQuery = true)
+    int memberCountWithPlansNameOf(@Param("name") String planName);
 
-        // return a list of members by specific name
-        @Query(value = "SELECT * FROM members " +
-                        "WHERE plan_name = :name", nativeQuery = true)
-        List<Member> getMemberListByPlanName(@Param("name") String name);
+    // return a list of members by specific name
+    @Query(value = "SELECT * FROM members " +
+            "WHERE plan_name = :name", nativeQuery = true)
+    List<Member> getMemberListByPlanName(@Param("name") String name);
 
-        // return a list members whose plan duration is left for certain days (as
-        // parameter)
-        @Query(value = "SELECT * FROM members " +
-                        "WHERE plan_duration_left = :duration", nativeQuery = true)
-        List<Member> getMemberListByDuration(@Param("duration") int duration);
+    // return a list members whose plan duration is left for certain days (as
+    // parameter)
+    @Query(value = "SELECT * FROM members " +
+            "WHERE plan_duration_left = :duration", nativeQuery = true)
+    List<Member> getMemberListByDuration(@Param("duration") int duration);
+
+    @Query(value = "SELECT * FROM members " +
+            "WHERE plan_expiration = :duration", nativeQuery = true)
+    List<Member> findAllByExpiredPlans(@Param("duration") LocalDateTime duration);
 }
