fe8c78bdd6fc96a1b97406a09ca9e963038cbc65
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-17 21:44:38 +0530

    add response dto to get all recent transaction

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/RecentTransactionsResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/RecentTransactionsResponseDto.java
@@ -1,4 +1,26 @@
 package com.gym.planService.Dtos.OrderDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecentTransactionsResponseDto {
+
+    private String paymentId;
+    private String userName;
+    private String userId;
+    private String planName;
+    private Double paidPrice;
+    private String paymentStatus;
+    private String paymentMethod;
+    private LocalDate paymentDate;
+    private LocalDateTime  paymentTime;
 }
