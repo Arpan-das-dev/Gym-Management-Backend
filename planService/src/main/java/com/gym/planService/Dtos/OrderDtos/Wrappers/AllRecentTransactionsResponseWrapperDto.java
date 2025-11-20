05961ae89ae38ab78a60357b2e1df136026496a0
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:38:25 +0530

    add new dto for recent transaction and a wrapper to avoid redis serialization/deserilaztion issue

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Wrappers/AllRecentTransactionsResponseWrapperDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Wrappers/AllRecentTransactionsResponseWrapperDto.java
@@ -1,4 +1,23 @@
 package com.gym.planService.Dtos.OrderDtos.Wrappers;
 
+import com.gym.planService.Dtos.OrderDtos.Responses.RecentTransactionsResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllRecentTransactionsResponseWrapperDto {
+   private List<RecentTransactionsResponseDto> responseDtoList;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+
+    private boolean lastPage;
 }
