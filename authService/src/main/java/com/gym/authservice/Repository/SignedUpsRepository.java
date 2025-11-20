c8771909f834aad7b6ece697e290a493ed0ce758
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:25:05 +0530

    code refractored

--- authService/src/main/java/com/gym/authservice/Repository/SignedUpsRepository.java
+++ authService/src/main/java/com/gym/authservice/Repository/SignedUpsRepository.java
@@ -24,45 +24,15 @@ public interface SignedUpsRepository extends ReactiveCrudRepository<SignedUps, S
 
     @Query("""
     INSERT INTO signed_up (
-        gym_id,
-        first_name,
-        last_name,
-        gender,
-        user_mail,
-        phone_no,
-        password,
-        role,
-        joined_on,
-        is_verified_email,
-        is_verified_phone,
-        approved
-    ) VALUES (
-        :id,
-        :firstName,
-        :lastName,
-        :gender,
-        :email,
-        :phone,
-        :password,
-        :role,
-        :joinDate,
-        :isEmailVerified,
-        :isPhoneVerified,
-        :approved
+        gym_id, first_name, last_name, gender, user_mail, phone_no,
+        password, role, joined_on, is_verified_email, is_verified_phone, approved
+    ) VALUES ( :id, :firstName, :lastName, :gender, :email, :phone,
+        :password, :role, :joinDate, :isEmailVerified, :isPhoneVerified, :approved
     )
 """)
-    Mono<Void> insertSignedUp(String id,
-                              String firstName,
-                              String lastName,
-                              String gender,
-                              String email,
-                              String phone,
-                              String password,
-                              RoleType role,
-                              LocalDate joinDate,
-                              boolean isEmailVerified,
-                              boolean isPhoneVerified,
-                              boolean approved);
+    Mono<Void> insertSignedUp(String id, String firstName, String lastName, String gender, String email,
+                              String phone, String password, RoleType role, LocalDate joinDate,
+                              boolean isEmailVerified, boolean isPhoneVerified, boolean approved);
 
 
 
