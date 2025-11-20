c745ecc05b0843acc02b2d7b8c798d7bd0fbefc7
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:39:09 +0530

    add method to get filterd+paginated+searched+sorted data of recent transactions

--- planService/src/main/java/com/gym/planService/Repositories/PlanPaymentRepository.java
+++ planService/src/main/java/com/gym/planService/Repositories/PlanPaymentRepository.java
@@ -2,8 +2,11 @@ package com.gym.planService.Repositories;
 
 import com.gym.planService.Models.PlanPayment;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -22,4 +25,17 @@ public interface PlanPaymentRepository extends JpaRepository<PlanPayment,String>
             "WHERE p.paymentStatus = 'SUCCESS' AND p.paymentMonth =:month AND p.paymentYear = :year")
     List<PlanPayment> findByCurrentMonthAndYear(@Param("month") String month,
                                                 @Param("year") Integer year);
+
+    @Query("""
+        SELECT p FROM PlanPayment p
+        WHERE LOWER(p.userName) LIKE LOWER(CONCAT('%', :search, '%'))
+           OR LOWER(p.userId) LIKE LOWER(CONCAT('%', :search, '%'))
+           OR LOWER(p.planId) LIKE LOWER(CONCAT('%', :search, '%'))
+           OR CAST(p.paymentDate AS string) LIKE CONCAT('%', :search, '%')
+           OR CAST(p.transactionTime AS string) LIKE CONCAT('%', :search, '%')
+    """)
+    Page<PlanPayment> searchByUserNameOrDate(@Param("search") String search, Pageable pageable);
+
+
+
 }
