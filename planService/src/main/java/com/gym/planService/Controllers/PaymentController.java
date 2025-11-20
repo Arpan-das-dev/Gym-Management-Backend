228fdf9ca0f4712a4edba1717e3eca7cfc703608
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-20 17:37:34 +0530

    add endpoint to get recent transactions by admin

--- planService/src/main/java/com/gym/planService/Controllers/PaymentController.java
+++ planService/src/main/java/com/gym/planService/Controllers/PaymentController.java
@@ -3,6 +3,7 @@ package com.gym.planService.Controllers;
 import com.gym.planService.Dtos.OrderDtos.Requests.PlanPaymentRequestDto;
 
 import com.gym.planService.Dtos.OrderDtos.Requests.ConfirmPaymentDto;
+import com.gym.planService.Dtos.OrderDtos.Wrappers.AllRecentTransactionsResponseWrapperDto;
 import com.gym.planService.Dtos.PlanDtos.Responses.GenericResponse;
 import com.gym.planService.Services.PaymentService.PaymentService;
 import jakarta.validation.Valid;
@@ -13,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Slf4j
 @Validated
 @RestController
@@ -21,7 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
 
     private final PaymentService paymentService;
-
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     @PostMapping("/all/createOrder")
     public ResponseEntity<GenericResponse> createOrder(@Valid @RequestBody PlanPaymentRequestDto requestDto) {
         log.info("Request received to create order for Rs. {}", requestDto.getAmount());
@@ -39,5 +43,22 @@ public class PaymentController {
     }
 
     @GetMapping("/admin/getAllTransaction")
-    public ResponseEntity<>
+    public ResponseEntity<AllRecentTransactionsResponseWrapperDto> getRecentPayment(
+            @RequestParam(required = false) String searchBy,
+            @RequestParam(defaultValue = "date") String sortBy,
+            @RequestParam(defaultValue = "desc") String sortDirection,
+            @RequestParam int pageNo,
+            @RequestParam int pageSize
+    ) {
+        System.out.println("➡ Controller: getAllTransaction() at " + LocalDateTime.now().format(formatter));
+        pageNo = Math.max(pageNo, 0);
+        pageSize = pageSize< 0 ? 12 : pageSize;
+        log.info(
+                "Admin Transaction Request | page={} | size={} | searchBy='{}' | sortBy='{}' | direction='{}'",
+                pageNo, pageSize, searchBy, sortBy, sortDirection
+        );
+        AllRecentTransactionsResponseWrapperDto response = paymentService
+                .getPaginatedRecentTransactionWithSort(searchBy,sortBy,sortDirection,pageNo,pageSize);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 }
