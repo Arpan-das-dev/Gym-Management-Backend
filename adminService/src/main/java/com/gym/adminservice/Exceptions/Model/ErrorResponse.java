b75de1b7f100e58543fb64ff32ea89dc55a8f250
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 22:43:17 +0530

    build the error resoponse frome server exchange to httpServletRequest

--- adminService/src/main/java/com/gym/adminservice/Exceptions/Model/ErrorResponse.java
+++ adminService/src/main/java/com/gym/adminservice/Exceptions/Model/ErrorResponse.java
@@ -13,28 +13,9 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 
 public class ErrorResponse {
-    /**
-     * The exact time when the error occurred, represented in ISO‑8601 format.
-     */
     private LocalDateTime timestamp;
-
-    /**
-     * The HTTP status code representing the type of error (e.g., 404, 500).
-     */
     private int status;
-
-    /**
-     * The short error reason phrase or category (e.g., "Bad Request", "Internal Server Error").
-     */
     private String error;
-
-    /**
-     * A more detailed explanation or custom message describing the cause of the error.
-     */
     private String message;
-
-    /**
-     * The exact path or endpoint where the error originated, useful for debugging.
-     */
     private String path;
 }
