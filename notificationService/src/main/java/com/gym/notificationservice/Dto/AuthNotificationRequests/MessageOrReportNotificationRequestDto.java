a574c3db27d3495a719feb3648da69b255ee249c
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:10:52 +0530

    add dto to excecute the service class logic to notify user

--- notificationService/src/main/java/com/gym/notificationservice/Dto/AuthNotificationRequests/MessageOrReportNotificationRequestDto.java
+++ notificationService/src/main/java/com/gym/notificationservice/Dto/AuthNotificationRequests/MessageOrReportNotificationRequestDto.java
@@ -1,4 +1,16 @@
 package com.gym.notificationservice.Dto.AuthNotificationRequests;
 
-public class MessageOrReportNotificationResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageOrReportNotificationRequestDto {
+    private String sendTo;
+    private String subject;
+    private String message;
 }
