014ab1758a24612d739535351d26aac1ecc15a10
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 22:54:18 +0530

    add custom id gen util for reports or messages

--- adminService/src/main/java/com/gym/adminservice/Utils/ReportIdGenUtil.java
+++ adminService/src/main/java/com/gym/adminservice/Utils/ReportIdGenUtil.java
@@ -1,4 +1,13 @@
 package com.gym.adminservice.Utils;
 
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
 public class ReportIdGenUtil {
+    public String createMessageId(String userId, LocalDateTime messageTime){
+        return userId+messageTime.toString()+ ThreadLocalRandom.current().toString();
+    }
 }
