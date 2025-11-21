31fbbc09c4e271524a7c7cb15455484b734a724d
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:02:28 +0530

    add new response dtos for All MemberList response (used for get all members method and login Streak response to return member's current login streak and max login streak

--- memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Responses/LoginStreakResponseDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Responses/LoginStreakResponseDto.java
@@ -1,4 +1,15 @@
 package com.gym.member_service.Dto.MemberManagementDto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginStreakResponseDto {
+    private Integer logInStreak;
+    private Integer maxLogInStreak;
 }
