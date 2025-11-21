29458bc16b9a652289abbd129d65229005e672b4
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 22:51:05 +0530

    add repostory for message model for db interactions

--- adminService/src/main/java/com/gym/adminservice/Repository/MessageRepository.java
+++ adminService/src/main/java/com/gym/adminservice/Repository/MessageRepository.java
@@ -1,4 +1,13 @@
 package com.gym.adminservice.Repository;
 
-public interface MessageRepository {
+import com.gym.adminservice.Models.Messages;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Messages,String> {
+    @Query("SELECT m FROM Messages m WHERE userId = :userId")
+    List<Messages> findAllByUserId(@Param("userId") String userId);
 }
