529188eaa41ab7bce54d2427089209fc07f68243
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:09:54 +0530

    add controller to send mail/sms to user if their message/reports get resolved

--- notificationService/src/main/java/com/gym/notificationservice/Controller/MessageOrReportNotificationController.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/MessageOrReportNotificationController.java
@@ -1,4 +1,22 @@
 package com.gym.notificationservice.Controller;
 
+import com.gym.notificationservice.Dto.AuthNotificationRequests.MessageOrReportNotificationRequestDto;
+import com.gym.notificationservice.Services.AuthNotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("${messageOrReport.notification.url}")
+@RequiredArgsConstructor
 public class MessageOrReportNotificationController {
+
+    private AuthNotificationService notificationService;
+    @PostMapping("/resolved")
+    public String sendMessageStatusMailByService(MessageOrReportNotificationRequestDto requestDto){
+        return notificationService.sendMessageOrReportResolvedStatus(requestDto);
+    }
 }
