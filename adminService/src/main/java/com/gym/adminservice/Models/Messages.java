01b4aa847065a532dd9461f5f0c96593d3d641cc
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 22:44:04 +0530

    add model for message and report service

--- adminService/src/main/java/com/gym/adminservice/Models/Messages.java
+++ adminService/src/main/java/com/gym/adminservice/Models/Messages.java
@@ -1,4 +1,41 @@
 package com.gym.adminservice.Models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(indexes = {
+        @Index(name = "idx_user_id", columnList = "user_id"),
+        @Index(name = "idx_message_time", columnList = "message_time"),
+        @Index(name = "idx_message_time", columnList = "status")
+})
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Messages {
+
+    @Id
+    private String messageId;
+    @Column(nullable = false,name = "subject")
+    private String subject;
+    @Column(nullable = false, name = "user_id")
+    private String userId;
+    @Column(nullable = false, name = "user_name")
+    private String userName;
+    @Column(nullable = false,name = "user_Role")
+    private String userRole;
+    @Column(nullable = false,name = "email")
+    private String emailId;
+    @Column(nullable = false,name = "message")
+    private String message;
+    @Column(nullable = false, name = "status")
+    private String status;
+    @Column(nullable = false, name = "message_time")
+    private LocalDateTime messageTime;
 }
