2737f78f53557775837a739eae4dd6dfc19d55a9
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:03:25 +0530

    add wrappers for @AllMemberListResponseDto.java to avoid redis serialization/deserialization issue

--- memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Wrappers/AllMembersInfoWrapperResponseDtoList.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberManagementDto/Wrappers/AllMembersInfoWrapperResponseDtoList.java
@@ -1,4 +1,23 @@
 package com.gym.member_service.Dto.MemberManagementDto.Wrappers;
 
-public class AllMembersInfoResponseDtoList {
+import com.gym.member_service.Dto.MemberManagementDto.Responses.AllMemberListResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllMembersInfoWrapperResponseDtoList {
+    List<AllMemberListResponseDto> responseDtoList;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+
+    private boolean lastPage;
 }
