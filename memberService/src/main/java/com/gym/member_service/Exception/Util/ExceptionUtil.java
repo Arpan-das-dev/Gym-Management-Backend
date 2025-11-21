d6fe15fb05ecfb6392ab55b8275b699c65d0b548
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-21 23:04:45 +0530

    add utility method to build error response

--- memberService/src/main/java/com/gym/member_service/Exception/Util/ExceptionUtil.java
+++ memberService/src/main/java/com/gym/member_service/Exception/Util/ExceptionUtil.java
@@ -1,4 +1,25 @@
 package com.gym.member_service.Exception.Util;
 
+import com.gym.member_service.Exception.Model.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+
 public class ExceptionUtil {
+    private ExceptionUtil() {
+    }
+    public static ResponseEntity<ErrorResponse> buildErrorResponse(
+            HttpStatus status, String message, HttpServletRequest request) {
+
+        return ResponseEntity.status(status)
+                .body(new ErrorResponse(
+                        LocalDateTime.now(),
+                        status.value(),
+                        status.getReasonPhrase(),
+                        message,
+                        request.getRequestURI()
+                ));
+    }
 }
