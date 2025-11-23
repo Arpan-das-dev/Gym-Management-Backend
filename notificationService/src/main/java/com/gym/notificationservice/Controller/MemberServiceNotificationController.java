8fe0ed206fc98290e63fba3036309f4fcc9bb38c
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:20:47 +0530

    add endpoint to sent mail for member's account update(frozen/unfrozen)

--- notificationService/src/main/java/com/gym/notificationservice/Controller/MemberServiceNotificationController.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/MemberServiceNotificationController.java
@@ -1,17 +1,18 @@
 package com.gym.notificationservice.Controller;
 
 import com.gym.notificationservice.Dto.MailNotificationDtos.MailNotificationRequestDto;
+import com.gym.notificationservice.Dto.MailNotificationDtos.NotificationFrozenRequestDto;
 import com.gym.notificationservice.Dto.MailNotificationDtos.PlanActivationNotificationRequestDto;
 import com.gym.notificationservice.Services.MemberNotificationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequestMapping("${memberService.url}")
 @RequiredArgsConstructor
@@ -80,11 +81,16 @@ public class MemberServiceNotificationController {
      * to send notification.
      */
     @PostMapping("/activePlan")
-    public ResponseEntity<String> sendUpdatePLanNotification (@RequestBody @Valid PlanActivationNotificationRequestDto
-                                                      requestDto){
-        // using memberService to send sms/mail
+    public ResponseEntity<String> sendUpdatePLanNotification (
+            @RequestBody PlanActivationNotificationRequestDto requestDto) {
         memberNotificationService.sendPlanUpdateEmail(requestDto);
-        return ResponseEntity.status(HttpStatus.OK).body("Email send Successfully");
-        // if successfully sends mail then returns OK as http status
+        return ResponseEntity.status(HttpStatus.OK).body("Email sent Successfully");
+    }
+
+    @PostMapping("/accountStatus")
+    public ResponseEntity<String> sendAccountStatusInfo(@RequestBody NotificationFrozenRequestDto requestDto) {
+        log.info("Request received to update status for member {},",requestDto.getName());
+        String response =  memberNotificationService.sendAccountStatusUpdate(requestDto);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 }
