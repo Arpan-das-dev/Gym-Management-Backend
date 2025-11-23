c542830d08dbd322a69f58c363df80d007b05333
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:33:36 +0530

    add new end points to send payment emails with attachment(receipt)

--- notificationService/src/main/java/com/gym/notificationservice/Controller/PaymentNotificationController.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/PaymentNotificationController.java
@@ -1,13 +1,17 @@
 package com.gym.notificationservice.Controller;
 
+import com.gym.notificationservice.Dto.PaymentNotificationDtos.Requests.PlanNotificationRequest;
 import com.gym.notificationservice.Dto.PaymentNotificationDtos.Requests.PlanPaymentRequestDto;
 import com.gym.notificationservice.Dto.PaymentNotificationDtos.Responses.GenericResponseDto;
+import com.gym.notificationservice.Services.PaymentNotificationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class PaymentNotificationController {
 
+    private final PaymentNotificationService notificationService;
     @PostMapping("/all/createOrder")
     public ResponseEntity<GenericResponseDto> createOrder(@Valid @RequestBody PlanPaymentRequestDto requestDto) {
         log.info("");
@@ -26,4 +31,18 @@ public class PaymentNotificationController {
         log.info("");
         return ResponseEntity.status(HttpStatus.OK).body(new GenericResponseDto("sent"));
     }
+
+    @PostMapping(
+            value = "/all/sendAttachment",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<String> sendMailWithAttachment(
+            @ModelAttribute PlanNotificationRequest request,
+            @RequestParam("attachment") MultipartFile multipartFile) {
+
+        log.info("request received to send email with attachment");
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(notificationService.sendAttachedMail(request, multipartFile));
+    }
 }
+
