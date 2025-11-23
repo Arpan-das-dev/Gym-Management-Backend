394920cd544974631b9791704711f8a16085cd91
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:45:40 +0530

    impemented mailjet service

--- notificationService/src/main/java/com/gym/notificationservice/Services/AuthNotificationService.java
+++ notificationService/src/main/java/com/gym/notificationservice/Services/AuthNotificationService.java
@@ -1,19 +1,22 @@
 package com.gym.notificationservice.Services;
 
 import com.gym.notificationservice.Dto.AuthNotificationRequests.*;
-import jakarta.validation.Valid;
+import com.gym.notificationservice.Dto.PaymentNotificationDtos.Responses.GenericResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthNotificationService {
 
-    private final MailService mailService;
+//    private final MailService mailService;
     private final SmsService smsService;
     private final TemplateEngine templateEngine;
+    private final MailjetService mailjetService;
 
     public void sendWelcomeMail(WelcomeRequestDto requestDto){
         Context context = new Context();
@@ -23,7 +26,8 @@ public class AuthNotificationService {
         context.setVariable("phone",requestDto.getPhone());
 
         String body = templateEngine.process("welcome-mail",context);
-        mailService.sendMail(requestDto.getEmail(), "Welcome To FitStudio",body);
+//        mailService.sendMail(requestDto.getEmail(), "Welcome To FitStudio",body);
+        mailjetService.sendMail(requestDto.getEmail(), "Welcome To FitStudio",body);
     }
 
     public void SendCredentials(SendCredentialRequestDto requestDto) {
@@ -32,7 +36,8 @@ public class AuthNotificationService {
         context.setVariable("password",requestDto.getPassword());
 
         String body = templateEngine.process("send-credentials",context);
-        mailService.sendMail(requestDto.getEmail(), "User Credentials",body);
+//        mailService.sendMail(requestDto.getEmail(), "User Credentials",body);
+        mailjetService.sendMail(requestDto.getEmail(), "User Credentials",body);
     }
 
 
@@ -42,14 +47,13 @@ public class AuthNotificationService {
         context.setVariable("otp",requestDto.getOtp());
 
         String body = templateEngine.process("verify-email",context);
-        mailService.sendMail(requestDto.getEmail(), "Email Verification",body);
+//        mailService.sendMail(requestDto.getEmail(), "Email Verification",body);
+        mailjetService.sendMail(requestDto.getEmail(), "Email Verification",body);
     }
 
 
     public void sendVerifyPhone(PhoneOtpRequestDto requestDto) {
-        String sms = "Hello,"+ requestDto.getName()+ " Your FitStudio verification code is " +requestDto.getOtp() +
-                " It is valid for 15 minutes. Do not share this code with anyone.";
-        smsService.sendSms(requestDto.getPhone(), sms);
+        smsService.sendOtp(requestDto.getPhone(),requestDto.getPhone());
     }
 
     public void sendResetPasswordEmail( EmailOtpRequestDto requestDto) {
@@ -58,10 +62,14 @@ public class AuthNotificationService {
         context.setVariable("otp",requestDto.getOtp());
 
         String body = templateEngine.process("reset-password",context);
-        mailService.sendMail(requestDto.getEmail(), "Email Verification For Password Reset",body);
+//        mailService.sendMail(requestDto.getEmail(), "Email Verification For Password Reset",body);
+        mailjetService.sendMail(requestDto.getEmail(), "Email Verification For Password Reset",body);
     }
 
-    public String sendMessageOrReportResolvedStatus(MessageOrReportNotificationRequestDto requestDto) {
-       return mailService.sendMail(requestDto.getSendTo(), requestDto.getSubject(), requestDto.getMessage());
+    public GenericResponseDto sendMessageOrReportResolvedStatus(MessageOrReportNotificationRequestDto requestDto) {
+//       return mailService.sendMail(requestDto.getSendTo(), requestDto.getSubject(), requestDto.getMessage());
+        log.info("sending to {}", requestDto.getSendTo());
+        mailjetService.sendMail(requestDto.getSendTo(), requestDto.getSubject(), requestDto.getMessage());
+        return new GenericResponseDto("Mail sent successfully to user for deleting account");
     }
 }
