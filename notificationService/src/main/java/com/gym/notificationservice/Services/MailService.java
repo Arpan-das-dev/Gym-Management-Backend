04cf57d8e4c0200af8520ca34e32a631bfc997f3
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:47:11 +0530

    change the return type from void to complitableFuture

--- notificationService/src/main/java/com/gym/notificationservice/Services/MailService.java
+++ notificationService/src/main/java/com/gym/notificationservice/Services/MailService.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -26,22 +27,27 @@ public class MailService {
     }
 
     @Async
-    public String  sendMail(String to, String subject, String body) {
+    public CompletableFuture<String> sendMail(String to, String subject, String body) {
         Email from = new Email(sender);
         Email sendTo = new Email(to);
         Content content = new Content("text/html", body);
         Request request = new Request();
         Mail mail = new Mail(from, subject, sendTo, content);
+
         try {
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
             Response response = sendGrid.api(request);
             System.out.println("SendGrid Status: " + response.getStatusCode());
-            return "Sent mail to {}"+to;
+
+            String result = "Sent mail to " + to + " with status: " + response.getStatusCode();
+            return CompletableFuture.completedFuture(result);
+
         } catch (IOException e) {
             log.warn(e.getLocalizedMessage());
-            return "unable to sent mail due to"+e.getLocalizedMessage();
+            String errorResult = "Unable to sent mail due to " + e.getLocalizedMessage();
+            return CompletableFuture.completedFuture(errorResult);
         }
     }
 
