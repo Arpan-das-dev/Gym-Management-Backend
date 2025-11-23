cf9f5a5feaf4ce09948d54034abe00f7afc6b447
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:55:02 +0530

    updated method

--- notificationService/src/main/java/com/gym/notificationservice/Services/SmsService.java
+++ notificationService/src/main/java/com/gym/notificationservice/Services/SmsService.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class SmsService {
@@ -21,28 +21,28 @@ public class SmsService {
         this.apiKey = apiKey;
     }
 
-    @Async
-    public void sendSms(String to, String message) {
-        try {
-            String url = "https://www.fast2sms.com/dev/bulkV2";
-            RestTemplate restTemplate = new RestTemplate();
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("Authorization", apiKey);
-            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    private final RestTemplate restTemplate = new RestTemplate();
+    public void sendOtp(String mobileNumber, String otp) {
 
-            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-            body.add("route", "t");
-            body.add("message", message);
-            body.add("language", "english");
-            body.add("flash", "0");
-            body.add("numbers", to);
+        String url = "https://www.fast2sms.com/dev/bulkV2";
 
-            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
-            restTemplate.postForEntity(url, request, String.class);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("authorization", apiKey);
 
-        } catch (Exception e) {
-            throw new RuntimeException("SMS sending failed", e);
-        }
+        Map<String, Object> body = new HashMap<>();
+        body.put("route", "v3");
+        body.put("sender_id", "XXXXX");     // your DLT sender ID
+        body.put("message", "123456");      // your DLT template ID
+        body.put("variables_values", otp);
+        body.put("numbers", mobileNumber);
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+
+        String response = restTemplate.postForObject(url, entity, String.class);
+
+        System.out.println("Fast2SMS Response = " + response);
     }
 }
+
