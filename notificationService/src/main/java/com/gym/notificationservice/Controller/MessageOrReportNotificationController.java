75b635f2c66234d86698da6bb13318b8e067e696
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:30:52 +0530

    add new controller to send any message or reports

--- notificationService/src/main/java/com/gym/notificationservice/Controller/MessageOrReportNotificationController.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/MessageOrReportNotificationController.java
@@ -1,12 +1,21 @@
 package com.gym.notificationservice.Controller;
 
 import com.gym.notificationservice.Dto.AuthNotificationRequests.MessageOrReportNotificationRequestDto;
+import com.gym.notificationservice.Dto.PaymentNotificationDtos.Responses.GenericAsyncResponseDto;
+import com.gym.notificationservice.Dto.PaymentNotificationDtos.Responses.GenericResponseDto;
 import com.gym.notificationservice.Services.AuthNotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RestController
@@ -14,9 +23,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MessageOrReportNotificationController {
 
-    private AuthNotificationService notificationService;
+    private final  AuthNotificationService notificationService;
     @PostMapping("/resolved")
-    public String sendMessageStatusMailByService(MessageOrReportNotificationRequestDto requestDto){
-        return notificationService.sendMessageOrReportResolvedStatus(requestDto);
+    public ResponseEntity<GenericResponseDto> sendMessageStatusMailByService(MessageOrReportNotificationRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(notificationService.sendMessageOrReportResolvedStatus(requestDto));
+    }
+    @PostMapping("/sendMail")
+    public ResponseEntity<GenericResponseDto> sendMailToMember(
+           @RequestBody MessageOrReportNotificationRequestDto requestDto){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(notificationService.sendMessageOrReportResolvedStatus(requestDto));
     }
 }
