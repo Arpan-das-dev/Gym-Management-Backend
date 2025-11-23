beb6d456ccd4679e4ba8389cbf7128ca1a918d6f
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:35:43 +0530

    add test controller(dev only)

--- notificationService/src/main/java/com/gym/notificationservice/Controller/test.java
+++ notificationService/src/main/java/com/gym/notificationservice/Controller/test.java
@@ -1,4 +1,21 @@
 package com.gym.notificationservice.Controller;
 
+import com.gym.notificationservice.Services.SmsService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/test-me")
+@RequiredArgsConstructor
 public class test {
+
+    private final SmsService smsService;
+    @PostMapping("/now")
+    public void sendSms(){
+        smsService.sendOtp("8145415374","BApKeLauDe");
+    }
 }
