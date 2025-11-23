5dce9c7b87f5b09d22f67688dd58b4de1fe6307b
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:19:46 +0530

    add mailjet configurations

--- notificationService/src/main/java/com/gym/notificationservice/Config/MailJetConfig.java
+++ notificationService/src/main/java/com/gym/notificationservice/Config/MailJetConfig.java
@@ -1,4 +1,30 @@
 package com.gym.notificationservice.Config;
 
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MailJetConfig {
+
+    private final String api_KEY;
+    private final String secret_key;
+
+    public MailJetConfig(@Value("${mailjet.api.key}") String api_KEY,
+                         @Value("${mailjet.secret.key}") String secret_key)
+    {
+        this.api_KEY = api_KEY;
+        this.secret_key = secret_key;
+    }
+
+    @Bean
+    public MailjetClient mailjetClient (){
+        ClientOptions options = ClientOptions.builder()
+                .apiKey(api_KEY)
+                .apiSecretKey(secret_key)
+                .build();
+        return new MailjetClient(options);
+    }
 }
