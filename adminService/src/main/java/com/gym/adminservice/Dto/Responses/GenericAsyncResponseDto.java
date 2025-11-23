5565b7aac88515aff413b434a85fe80b4b1e7052
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:08:37 +0530

    ädd response dto for async calls

--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/GenericAsyncResponseDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/GenericAsyncResponseDto.java
@@ -1,4 +1,13 @@
 package com.gym.adminservice.Dto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GenericAsyncResponseDto {
+    Mono<String> message;
 }
