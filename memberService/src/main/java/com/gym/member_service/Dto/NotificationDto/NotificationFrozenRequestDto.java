61be08270d648d4d9fb42d9a277a9d3248e17c26
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 20:14:07 +0530

    add dtos for member to send to webclient when account frozen/unfrozen or new plan triggerd

--- memberService/src/main/java/com/gym/member_service/Dto/NotificationDto/NotificationFrozenRequestDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/NotificationDto/NotificationFrozenRequestDto.java
@@ -1,4 +1,20 @@
 package com.gym.member_service.Dto.NotificationDto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationFrozenRequestDto {
+
+    private String name;
+    private String mailId;
+    private LocalDate frozenDate;
+    private boolean frozen;
 }
