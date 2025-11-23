f4352958691dec743b68b82ce62b6dd159e1e23d
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-23 21:04:08 +0530

    update dtos

--- planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Responses/CuponCodeResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/CuponDtos/Responses/CuponCodeResponseDto.java
@@ -43,6 +43,7 @@ public class CuponCodeResponseDto {
      */
     private String cuponCode;
     private String planId;
+    private String planName;
     private String access;
     private LocalDate validFrom;
     /**
--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanResponseDtoForMemberService.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanResponseDtoForMemberService.java
@@ -1,4 +1,23 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanResponseDtoForMemberService {
+    @NotBlank(message = "plan id is required")
+    private String planId;
+
+    @NotBlank(message = "plan name is required")
+    private String planName;
+
+    @NotNull(message = "Duration can not be empty")
+    private Integer duration;
 }
