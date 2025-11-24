2e0ab9823dbb1a33513b9149cc11c0af63c7dd32
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-24 17:31:50 +0530

    add dto to send response in string format

---
+++ authService/src/main/java/com/gym/authservice/Dto/Response/GenericResponseDto.java
@@ -0,0 +1,14 @@
+package com.gym.authservice.Dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GenericResponseDto {
+    String message;
+}
