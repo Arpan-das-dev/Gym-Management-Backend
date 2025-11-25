b50069c5c1683d304efe181dbf025ff6c49adb0e
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-25 22:46:00 +0530

    add new dtos to send member's basic profile and member's plan details

--- memberService/src/main/java/com/gym/member_service/Dto/MemberPlanDto/Responses/MemberPlanInfoResponseDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberPlanDto/Responses/MemberPlanInfoResponseDto.java
@@ -1,4 +1,19 @@
 package com.gym.member_service.Dto.MemberPlanDto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberPlanInfoResponseDto {
+    private LocalDateTime planExpiration;
+    private String planId;
+    private String planName;
+    private Integer planDurationLeft;
 }
