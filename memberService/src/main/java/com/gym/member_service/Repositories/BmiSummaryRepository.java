8cf0b423f6a5fe823260cdefae64bf4ab0335767
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-26 23:38:19 +0530

    update native sql queries to jpql query

--- memberService/src/main/java/com/gym/member_service/Repositories/BmiSummaryRepository.java
+++ memberService/src/main/java/com/gym/member_service/Repositories/BmiSummaryRepository.java
@@ -1,12 +1,15 @@
 package com.gym.member_service.Repositories;
 
 import com.gym.member_service.Model.BmiSummary;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface BmiSummaryRepository extends JpaRepository<BmiSummary,Long> {
-    List<BmiSummary> findByMemberId(String memberId);
 
+    Page<BmiSummary> findByMemberId(String memberId, Pageable pageable);
 }
