19e8b2e7430c0f4a7fc3d41fa24082af83448221
Arpan-das-dev <dasarpan915@gmail.com>
2025-11-26 23:33:30 +0530

    created a custom annotation to do the same repeateating logs

--- memberService/src/main/java/com/gym/member_service/Utils/LogExecutionTime.java
+++ memberService/src/main/java/com/gym/member_service/Utils/LogExecutionTime.java
@@ -1,4 +1,11 @@
 package com.gym.member_service.Utils;
 
-public @interface LogExecutionTim {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LogExecutionTime {
 }
