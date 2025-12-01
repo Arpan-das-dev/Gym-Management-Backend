35d8deedf57d7401a7f17b22342b5c30ddabea7a
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-01 21:38:40 +0530

    add new dto for creating a new trainer in trainer service after get approved by admin

--- authService/src/main/java/com/gym/authservice/Dto/Request/TrainerCreateRequestDto.java
+++ authService/src/main/java/com/gym/authservice/Dto/Request/TrainerCreateRequestDto.java
@@ -1,4 +1,29 @@
 package com.gym.authservice.Dto.Request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrainerCreateRequestDto {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+    private String gender;
+
+    private LocalDate joinDate;
 }
