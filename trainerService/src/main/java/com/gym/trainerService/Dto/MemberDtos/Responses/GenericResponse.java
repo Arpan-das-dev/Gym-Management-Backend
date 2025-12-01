f7d97c65090e5e012c0ae22599ab79dc063f5742
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-01 21:48:48 +0530

    add new response type which contains only one feild @Message

--- trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Responses/GenericResponse.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/MemberDtos/Responses/GenericResponse.java
@@ -1,4 +1,14 @@
 package com.gym.trainerService.Dto.MemberDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GenericResponse {
+    private String message;
 }
