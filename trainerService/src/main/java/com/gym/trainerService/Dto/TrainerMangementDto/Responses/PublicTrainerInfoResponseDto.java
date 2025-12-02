ea1834e701768555c47e374ba4e6360a674ba86e
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-02 23:17:18 +0530

    add new feilds eg @about @specilites @review count @member Count

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Responses/PublicTrainerInfoResponseDto.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerMangementDto/Responses/PublicTrainerInfoResponseDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -13,7 +15,11 @@ public class PublicTrainerInfoResponseDto {
     private String id;
     private String firstName;
     private String lastName;
+    private String about;
+    private int clientCount;
     private String email;
     private String gender;
     private double averageRating;
+    private int reviewCount;
+    List<String> specialities;
 }
