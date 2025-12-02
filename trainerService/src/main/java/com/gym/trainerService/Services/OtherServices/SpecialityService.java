5c754c449362ccd63528a33bc757d87c053e97a8
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-02 23:10:58 +0530

    changed the data type of return type for adding/delete/update trainer's speciality

--- trainerService/src/main/java/com/gym/trainerService/Services/OtherServices/SpecialityService.java
+++ trainerService/src/main/java/com/gym/trainerService/Services/OtherServices/SpecialityService.java
@@ -1,4 +1,40 @@
 package com.gym.trainerService.Services.OtherServices;
 
+import com.gym.trainerService.Dto.TrainerMangementDto.Requests.SpecialityResponseDto;
+import com.gym.trainerService.Enums.TrainerSpeciality;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class SpecialityService {
+
+    private static  final Set<String>VALID_SPECIALITIES =
+            Arrays.stream(TrainerSpeciality.values())
+                    .map(Enum::name)
+                    .collect(Collectors.toSet());
+
+    /**
+     * @param speciality which is all enums of speciality
+     * @return boolean value
+     */
+    public boolean isValidSpeciality(String speciality) {
+        return VALID_SPECIALITIES.contains(speciality);
+    }
+
+    public String normalize(String speciality) {
+        return speciality.trim().toUpperCase().replace(" ", "_");
+    }
+
+    @Cacheable(value = "allSpeciality", key = "'all'")
+    public SpecialityResponseDto getAllSpecialites() {
+        return SpecialityResponseDto.builder()
+                .specialityList(VALID_SPECIALITIES.stream().map(this::normalize).toList())
+                .build();
+    }
 }
