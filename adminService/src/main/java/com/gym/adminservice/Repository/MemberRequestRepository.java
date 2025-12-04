ec57454ca523f3d9f28e050818167cb9d94a0f0c
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-04 20:14:12 +0530

    add a new method with cusotm jpql query to use it in service class

--- adminService/src/main/java/com/gym/adminservice/Repository/MemberRequestRepository.java
+++ adminService/src/main/java/com/gym/adminservice/Repository/MemberRequestRepository.java
@@ -1,9 +1,15 @@
 package com.gym.adminservice.Repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gym.adminservice.Models.MemberRequest;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRequestRepository extends JpaRepository<MemberRequest,String> {
-    
+
+    @Query("SELECT m FROM MemberRequest m WHERE m.memberId =:memberId")
+    Optional<MemberRequest> findBYMemberId(@Param("memberId") String memberId);
 }
