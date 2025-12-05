0ec9309b5a3dd19c8bf799d2859f5494d6c7ef09
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-05 21:51:15 +0530

    add new response dto to send about trainer's dashboard summary

--- trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Responses/RatingMatrixInfo.java
+++ trainerService/src/main/java/com/gym/trainerService/Dto/TrainerReviewDto/Responses/RatingMatrixInfo.java
@@ -1,4 +1,16 @@
 package com.gym.trainerService.Dto.TrainerReviewDto.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RatingMatrixInfo {
+    private double currentRating;
+    private double oldRating;
+    private double change;
 }
