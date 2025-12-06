912beca444584cb59fa7c306082b87e7119b65d4
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-06 21:01:28 +0530

    add new custom exception when communication error happens between two micro service

--- trainerService/src/main/java/com/gym/trainerService/Exception/Custom/InterServiceCommunicationError.java
+++ trainerService/src/main/java/com/gym/trainerService/Exception/Custom/InterServiceCommunicationError.java
@@ -1,7 +1,7 @@
 package com.gym.trainerService.Exception.Custom;
 
 public class InterServiceCommunicationError extends RuntimeException {
-  public InterServiceCommunicationError(String message) {
-    super(message);
-  }
+    public InterServiceCommunicationError(String message) {
+        super(message);
+    }
 }
