4de6148f9e15665b1a623e0edb3bca485a442516
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-06 20:54:04 +0530

    add new class with a method to evict cache of paginated datas

--- trainerService/src/main/java/com/gym/trainerService/Utils/CustomCacheEvict.java
+++ trainerService/src/main/java/com/gym/trainerService/Utils/CustomCacheEvict.java
@@ -1,4 +1,67 @@
 package com.gym.trainerService.Utils;
 
-public class CustomCacheEvicter {
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class CustomCacheEvict{
+    private final StringRedisTemplate redisTemplate;
+    public void evictTrainerSessionCachePattern(String cacheName, String trainerId, String type) {
+
+        Set<String> keysToDelete = new HashSet<>();
+
+        // MEMBER-FIRST caches (e.g., BMI)
+        String trainerPattern = cacheName + "::" + trainerId + "*";
+
+        // Session caches
+        String upPattern   = cacheName + "::UP:" + trainerId + "*";
+        String pastPattern = cacheName + "::PAST:" + trainerId + "*";
+
+        log.debug("Evicting cache '{}' for trainer '{}' with type '{}'", cacheName, trainerId, type);
+
+        switch (type.toUpperCase()) {
+
+            case "UP":
+                keysToDelete.addAll(safeKeys(upPattern));
+                break;
+
+            case "PAST":
+                keysToDelete.addAll(safeKeys(pastPattern));
+                break;
+
+            case "MEMBER":
+                keysToDelete.addAll(safeKeys(trainerPattern));
+                break;
+
+            case "ALL":  // Clear every related cache
+                keysToDelete.addAll(safeKeys(trainerPattern));
+                keysToDelete.addAll(safeKeys(upPattern));
+                keysToDelete.addAll(safeKeys(pastPattern));
+                break;
+
+            default:
+                log.warn("Invalid cache eviction type '{}'. Allowed: UP, PAST, MEMBER, ALL", type);
+                return;
+        }
+
+        if (!keysToDelete.isEmpty()) {
+            redisTemplate.delete(keysToDelete);
+            log.info("Evicted {} keys from cache '{}' for trainer '{}'", keysToDelete.size(), cacheName, trainerId);
+        } else {
+            log.warn("No keys found to evict for cache '{}' and trainer '{}'", cacheName, trainerId);
+        }
+    }
+
+    private Set<String> safeKeys(String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        return (keys != null) ? keys : Collections.emptySet();
+    }
 }
