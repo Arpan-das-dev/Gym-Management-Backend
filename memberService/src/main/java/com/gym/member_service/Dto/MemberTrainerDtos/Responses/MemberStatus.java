281e50ed2a1381038e84dd933a46508e13b23bd4
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-06 20:55:25 +0530

    add new response dto to send chunk of member's status data for trainer

--- memberService/src/main/java/com/gym/member_service/Dto/MemberTrainerDtos/Responses/MemberStatus.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberTrainerDtos/Responses/MemberStatus.java
@@ -1,4 +1,15 @@
 package com.gym.member_service.Dto.MemberTrainerDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberStatus {
+    private String memberId;
+    private boolean active;
 }
