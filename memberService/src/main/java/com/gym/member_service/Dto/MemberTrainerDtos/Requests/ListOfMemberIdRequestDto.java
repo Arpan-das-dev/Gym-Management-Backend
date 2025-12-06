1bf856346523e73839caf799c7041da094886e43
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-06 20:54:50 +0530

    add new dto to get chunks of member's status (online/offline)

--- memberService/src/main/java/com/gym/member_service/Dto/MemberTrainerDtos/Requests/ListOfMemberIdRequestDto.java
+++ memberService/src/main/java/com/gym/member_service/Dto/MemberTrainerDtos/Requests/ListOfMemberIdRequestDto.java
@@ -1,4 +1,16 @@
 package com.gym.member_service.Dto.MemberTrainerDtos.Requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ListOfMemberIdRequestDto {
+    private List<String> memberIds;
 }
