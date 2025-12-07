eb67551158c3ba66abc1dffba5600c1ac5fa81ac
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-07 21:02:49 +0530

    add methods to set status of a session and also add method to evict cache accroding to the requirements

--- trainerService/src/main/java/com/gym/trainerService/Services/MemberServices/SessionManagementService.java
+++ trainerService/src/main/java/com/gym/trainerService/Services/MemberServices/SessionManagementService.java
@@ -14,14 +14,15 @@ import com.gym.trainerService.Repositories.MemberRepository;
 import com.gym.trainerService.Repositories.SessionRepository;
 import com.gym.trainerService.Repositories.TrainerRepository;
 import com.gym.trainerService.Services.OtherServices.WebClientService;
+import com.gym.trainerService.Utils.CustomAnnotations.Annotations.LogRequestTime;
 import com.gym.trainerService.Utils.CustomCacheEvict;
 import com.gym.trainerService.Utils.SessionIdGenUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
@@ -37,6 +38,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Service class managing business logic for handling training sessions between trainers and members.
@@ -94,7 +96,11 @@ public class SessionManagementService {
      * @return {@link AllSessionsWrapperDto} containing updated list of sessions for the trainer
      */
     @Transactional
-    @CacheEvict(value = "sessionMatrix",key = "#trainerId")
+    @LogRequestTime
+    @Caching(evict = {
+            @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
+            @CacheEvict(value = "sessionMatrix",key = "#trainerId")
+    })
     public String addSession(String trainerId, String status, AddSessionRequestDto requestDto) {
         // validating trainer existence if not present then throw a custom exception */
         if(!trainerRepository.existsById(trainerId)) {
@@ -175,15 +181,20 @@ public class SessionManagementService {
      * @return updated {@link AllSessionsWrapperDto} for the trainer
      */
     @Transactional
-    @CachePut(value = "AllSessionCache", key = "#requestDto.trainerId")
-    @CacheEvict(value = "sessionMatrix",key = "#trainerId")
-    public AllSessionsWrapperDto updateSession(String sessionId, UpdateSessionRequestDto requestDto) {
+    @LogRequestTime
+    @Caching(evict = {
+            @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
+            @CacheEvict(value = "sessionMatrix",key = "#trainerId")
+    })
+    public String  updateSession(String sessionId, UpdateSessionRequestDto requestDto) {
+        // fetching trainer to check authenticity and if he/she is permissible to do so
+        Trainer trainer = getById(requestDto.getTrainerId());
         // fetching session from database if not found then throw custom exception
         Session session = sessionRepository.findById(sessionId)
                 .orElseThrow(()-> new NoSessionFoundException("No session found with this id: "+sessionId));
         log.info("Successfully retrieved session of id: {} from database",session.getSessionId());
         // validating session with trainerId and memberId if not matches then throw exception */
-        if(! session.getTrainerId().equals(requestDto.getTrainerId()) ||
+        if(! session.getTrainerId().equals(trainer.getTrainerId()) ||
         !session.getMemberId().equals(requestDto.getMemberId())) {
             log.warn("Invalid credentials for trainer or member id");
             throw new InvalidSessionException("session does not matches with trainer or member");
@@ -191,26 +202,55 @@ public class SessionManagementService {
         // calculating the start and end time */
         LocalDateTime startTime = requestDto.getSessionDate();
         LocalDateTime endTime = startTime.plusMinutes(Math.round(requestDto.getDuration() * 60));
-        Session sessionCheck = sessionRepository.sessionSlotCheck(startTime,endTime).orElseThrow();
+        Optional<Session> slotCheck = sessionRepository.sessionSlotCheck(startTime,endTime);
         // checking for overlapping sessions if overlapping found with different session id then throw exception */
-        if(!sessionCheck.getSessionId().equals(session.getSessionId())) {
-            log.warn("Wrong slot no empty slot found between {} and {} "
-                    ,startTime.format(formatter),endTime.format(formatter));
-            throw new InvalidSessionException("No empty slot is available between "+startTime.format(formatter) +
-                    " & "+ endTime.format(formatter));
+        if (slotCheck.isPresent()) {
+            Session sessionCheck = slotCheck.get();
+
+            // Check if the overlapping session found has a DIFFERENT ID than the session being updated.
+            // If it's the same session, it's just a time update on the current session, which is fine.
+            if (!sessionCheck.getSessionId().equals(sessionId)) {
+                log.warn("Wrong slot, overlapping session found (ID: {}) between {} and {} ",
+                        sessionCheck.getSessionId(), startTime.format(formatter), endTime.format(formatter));
+                // The original requirement: "already u have a session in that window"
+                throw new InvalidSessionException(
+                        "Another session is already scheduled during the requested slot (" +
+                                sessionCheck.getSessionName() +
+                                " from " + sessionCheck.getSessionStartTime().format(formatter) +
+                                " to " + sessionCheck.getSessionEndTime().format(formatter) +
+                                ")"
+                );
+            }
+            // If the sessionCheck ID matches the current sessionId, we proceed with the update.
         }
         // updating session details */
         session.setSessionName(requestDto.getSessionName());
         session.setSessionStartTime(startTime);
         session.setSessionEndTime(endTime);
-        // saving session to database */
-        sessionRepository.save(session);
         log.info("Session updated with name {} between {} - {}", session.getSessionName(),
                 session.getSessionStartTime().format(formatter),session.getSessionEndTime().format(formatter));
         // sending session details to member service by webClientService */
-        webClientService.updateSessionToMember(session);
+
         log.info("Dto send to webClient service for further process");
-        return responseDtoBuilderForAllSessionForTrainer(session.getTrainerId());
+        customCacheEvict.evictTrainerSessionCachePattern("AllSessionCache",session.getTrainerId(),"UP");
+        Mono<String> webClientResponse = webClientService.updateSessionToMember(session);
+        try {
+            String response;
+            response = webClientResponse
+                    .doOnSuccess(msg -> {
+                        sessionRepository.save(session);
+                        customCacheEvict
+                                .evictTrainerSessionCachePattern("AllSessionCache",session.getTrainerId(),"UP");
+                        log.info("Successfully Updated session to database after Member Service confirmation on {}",
+                                session.getSessionStartTime().format(formatter));
+                    })
+                    .block();
+
+            return response;
+        } catch (RuntimeException e) {
+            log.error("Failed to add session after WebClient call. Session not saved locally.", e);
+            throw e;
+        }
     }
 
     /**
@@ -223,16 +263,11 @@ public class SessionManagementService {
      * @param trainerId unique identifier of the trainer
      * @return {@link AllSessionsWrapperDto} containing all upcoming sessions
      */
+    @LogRequestTime
     @Cacheable(value = "AllSessionCache", key = "'UP:' + #trainerId + ':' + #pageNo + ':' + #pageSize")
     public AllSessionsWrapperDto getUpcomingSessions(String trainerId, int pageNo, int pageSize) {
         log.info("Request received in service class for all upcoming sessions for trainer {}", trainerId);
-        Trainer trainer = trainerRepository.findById(trainerId)
-                .orElseThrow(() -> new NoTrainerFoundException(
-                        "No trainer found with the id: " + trainerId));
-        log.info("successfully retrieved sessions form database for trainer {}", trainer.getTrainerId());
-        if (trainer.isFrozen()) {
-            throw new UnAuthorizedRequestException("Your Account has been Suspended Please Contact Admin");
-        }
+        getById(trainerId);
         Sort sort = Sort.by(Sort.Direction.ASC, "sessionStartTime");
         Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
         Page<Session> sessionPage = sessionRepository.findAllUpcoming(LocalDateTime.now(), pageable);
@@ -266,7 +301,9 @@ public class SessionManagementService {
      * @param pageSize  number of records per page
      * @return {@link AllSessionsWrapperDto} containing paginated session data
      */
-    @Cacheable(value = "AllSessionCache", key = "#trainerId + ':' + #pageNo + ':' + #pageSize + ':' + #sortDirection")
+    @LogRequestTime
+    @Cacheable(value = "AllSessionCache",
+            key = "'PAST:' + #trainerId + ':' + #pageNo + ':' + #pageSize + ':' + #sortDirection")
     public AllSessionsWrapperDto getPastSessionsByPagination(String trainerId, int pageNo, int pageSize,String sortDirection)
     {
         log.info("Request received in service class for past sessions for size {} and page no {}",pageSize,pageNo);
@@ -321,8 +358,10 @@ public class SessionManagementService {
      * @param trainerId ID of the trainer performing the deletion
      * @return success message after deletion
      */
+    @LogRequestTime
     @Transactional
     @Caching(evict = {
+            @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
             @CacheEvict(value = "sessionMatrix",key = "#trainerId")
     })
     public String deleteSession(String sessionId,String trainerId) {
@@ -339,43 +378,30 @@ public class SessionManagementService {
         sessionRepository.deleteById(session.getSessionId());
         log.info("Successfully deleted session on {}",session.getSessionStartTime().format(formatter));
         // sending delete request to member service by webClientService */
-        webClientService.deleteSessionForMember(session.getSessionId(),session.getMemberId());
-        return "Successfully deleted session of id:: "+session.getSessionId();
+        Mono<String> webClientResponse =  webClientService
+                .deleteSessionForMember(session.getSessionId(),session.getMemberId());
+        try {
+            String response;
+            response = webClientResponse
+                    .doOnSuccess(msg -> {
+                        sessionRepository.save(session);
+                        customCacheEvict
+                                .evictTrainerSessionCachePattern("AllSessionCache",session.getTrainerId(),"UP");
+                        log.info("Successfully deleted session to database after Member Service confirmation on {}",
+                                session.getSessionStartTime().format(formatter));
+                    })
+                    .block();
+
+            return response;
+        } catch (RuntimeException e) {
+            log.error("Failed to add session after WebClient call. Session not saved locally.", e);
+            throw e;
+        }
     }
 
-    /**
-     * Helper method to construct {@link AllSessionsWrapperDto} containing a trainer’s sessions.
-     * <p>
-     * This is used by both create and update operations to return a fresh session list
-     * without additional queries from the controller layer.
-     * </p>
-     *
-     * @param trainerId unique identifier of the trainer
-     * @return wrapped response DTO containing list of session DTOs
-     */
-    private AllSessionsWrapperDto responseDtoBuilderForAllSessionForTrainer(String trainerId) {
-        log.info("Method handling to building response dto");
-        // fetching data from database */
-        List<Session> sessionList = sessionRepository.findByTrainerId(trainerId,LocalDateTime.now());
-        log.info("Successfully retrieved {} no of sessionList for trainer {} from database",sessionList.size(),trainerId);
-        // building response dto using stream with map*/
-        List<AllSessionResponseDto> responseDto = sessionList.stream().map(session ->
-                AllSessionResponseDto.builder()
-                .sessionId(session.getSessionId()).sessionName(session.getSessionName())
-                .memberId(session.getMemberId())
-                .sessionStartTime(session.getSessionStartTime())
-                .sessionEndTime(session.getSessionEndTime())
-                .build()).toList();
-        log.info("successfully build {} sessions as response", responseDto.size());
-
-        return AllSessionsWrapperDto.builder()
-                .responseDtoList(responseDto)
-                .build();
-    }
-
+    @LogRequestTime
     @Cacheable(value = "sessionMatrix",key = "#trainerId")
     public SessionMatrixInfo getSessionMatrix(String trainerId) {
-
         log.info("Starting session matrix calculation for trainer: {}", trainerId);
 
         LocalDateTime today = LocalDateTime.now();
@@ -406,5 +432,93 @@ public class SessionManagementService {
         log.info("Finished session matrix calculation for trainer {}. Result: Total={}, Remaining={}", trainerId, totalSessions, remainingSessions);
 
         return result;
+    }
+
+    private Trainer getById(String trainerId){
+        Trainer trainer = null;
+
+        Cache.ValueWrapper wrapper = Objects.requireNonNull(cacheManager.getCache("trainer")).get(trainerId);
+
+        if (wrapper != null) {
+            try {
+                trainer = (Trainer) wrapper.get();
+                log.info("Trainer {} {} found in cache", Objects.requireNonNull(trainer).getFirstName(), trainer.getLastName());
+            } catch (ClassCastException e) {
+                log.warn("Error occurred during cache cast for trainerId {}: {}", trainerId, e.getLocalizedMessage());
+            }
+        }
+
+        if (trainer == null) {
+            trainer = trainerRepository.findById(trainerId)
+                    .orElseThrow(()-> new NoTrainerFoundException("No Trainer Found with this id"));
+
+            log.info("Reloaded trainer {} {} from db", trainer.getFirstName(), trainer.getLastName());
+            Objects.requireNonNull(cacheManager.getCache("trainer")).put(trainer.getTrainerId(), trainer);
+        }
+
+        if(trainer.isFrozen()){
+            throw new UnAuthorizedRequestException("Your Account has been Suspended Please Contact Admin");
+        }
+
+        log.info("Session service returning {} {} by private method", trainer.getFirstName(), trainer.getLastName());
+        return trainer;
+    }
+
+    /**
+     * This method is responsible to set a status for a session
+     * @param sessionId to fetch a valid {@link Session} from db
+     * @param trainerId to validate the authority of trainer by
+     * @see SessionManagementService#getById(String trainerId)
+     * @param status to set the status of the session
+     *               evicts also the cache on the basis of session's time
+     * @see SessionManagementService#evictIt(Session)
+     * @return Success Message retrieved from member service
+     */
+    @Transactional
+    @LogRequestTime
+    public String setStatusForSession(String sessionId, String trainerId, String status) {
+        log.info("Request received in service class to update status for session {}", sessionId);
+        Trainer trainer = getById(trainerId);
+        log.info("Fetched details for --> {} {}", trainer.getFirstName(), trainer.getLastName());
+        Session session = sessionRepository.findById(sessionId)
+                .orElseThrow(() -> new NoSessionFoundException("No Session Found With this Session credentials"));
+        session.setStatus(status.toUpperCase());
+        Mono<String> webClientResponse = webClientService.updateSessionStatusForMember
+                (session.getSessionId(), session.getMemberId(), session.getTrainerId(), status);
+        try {
+            String response;
+            response = webClientResponse
+                    .doOnSuccess(msg -> {
+                        evictIt(session);
+                        sessionRepository.save(session);
+                        log.info("Successfully updated session to database after Member Service confirmation on {}",
+                                session.getSessionStartTime().format(formatter));
+                    }).block();
+            return response;
+        } catch (Exception exception) {
+            log.error("💀💀 Failed to update session status after WebClient call", exception);
+            throw exception;
+        }
+    }
+
+    /**
+     * a private method to evict cache for {@link Trainer} from cache
+     * @param session to validate the session is in the past or future
+     * @see Session
+     * @see Trainer
+     * also see the method how the custom eviction for cache works
+     * @see CustomCacheEvict#evictTrainerSessionCachePattern(String, String, String)
+     */
+    private void evictIt(Session session) {
+        log.info("Request received for evict session cache");
+        boolean isPresentOrFuture = session.getSessionStartTime().isAfter(LocalDateTime.now());
+
+        if (isPresentOrFuture) {
+            customCacheEvict
+                    .evictTrainerSessionCachePattern("AllSessionCache", session.getTrainerId(), "UP");
+        } else {
+            customCacheEvict
+                    .evictTrainerSessionCachePattern("AllSessionCache", session.getTrainerId(), "PAST");
+        }
     }
 }
