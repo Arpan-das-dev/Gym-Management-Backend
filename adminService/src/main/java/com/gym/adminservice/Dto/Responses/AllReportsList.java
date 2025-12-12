1e05d7ee41bbd57f05bb6c05bf6f8b8c0fc13b01
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-12 19:34:59 +0530

    add new feilds in the dto

--- adminService/src/main/java/com/gym/adminservice/Dto/Responses/AllReportsList.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Responses/AllReportsList.java
@@ -12,7 +12,9 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AllReportsList {
+    private String userId;
    private String userName;
+   private String userRole;
    private String subject;
    private String message;
    private LocalDateTime messageTime;
