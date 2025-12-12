b69e4d1e384125c80d0b8927365075c90edbae6b
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-12 20:04:24 +0530

    update methods with new custom jpql query

--- trainerService/src/main/java/com/gym/trainerService/Repositories/ReviewRepository.java
+++ trainerService/src/main/java/com/gym/trainerService/Repositories/ReviewRepository.java
@@ -73,4 +73,7 @@ public interface ReviewRepository extends JpaRepository<Review, String> {
     @Query("SELECT avg(r.review) FROM Review r WHERE r.trainer.id =:trainerId AND r.reviewDate< :lastMonth")
     Double getReviewBYTrainerIdWithDate( @Param("trainerId") String trainerId,
                                          @Param("lastMonth") LocalDateTime lastMonth);
+
+    @Query("SELECT r FROM Review r WHERE r.userId =:userId")
+    Page<Review> findByUserId( @Param("userId") String userId, Pageable page);
 }
