08584ecbef3895cdecef63753fdb47f25887b41a
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-12 20:04:53 +0530

    add new jpql query method to update trainer's average rating

--- trainerService/src/main/java/com/gym/trainerService/Repositories/TrainerRepository.java
+++ trainerService/src/main/java/com/gym/trainerService/Repositories/TrainerRepository.java
@@ -1,8 +1,18 @@
 package com.gym.trainerService.Repositories;
 
 import com.gym.trainerService.Models.Trainer;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TrainerRepository extends JpaRepository<Trainer,String > {
     boolean existsByEmail( String email);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Trainer t SET t.averageRating = :avg WHERE t.trainerId = :id")
+    int updateAverageRating(@Param("id") String id, @Param("avg") double avg);
+
 }
