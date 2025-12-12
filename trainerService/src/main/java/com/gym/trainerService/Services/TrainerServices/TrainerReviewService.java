94fb6e38a8159667ef814fd65f6642a663ccacb5
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-12 20:07:27 +0530

    update the methods with new query and added custom java evict with the help of cache manger and load the trainer in the cache also added a anotation to track when the request reached

--- trainerService/src/main/java/com/gym/trainerService/Services/TrainerServices/TrainerReviewService.java
+++ trainerService/src/main/java/com/gym/trainerService/Services/TrainerServices/TrainerReviewService.java
@@ -8,15 +8,21 @@ import com.gym.trainerService.Dto.TrainerReviewDto.Wrapper.AllReviewResponseWrap
 import com.gym.trainerService.Exception.Custom.InvalidReviewException;
 import com.gym.trainerService.Exception.Custom.NoReviewFoundException;
 import com.gym.trainerService.Exception.Custom.NoTrainerFoundException;
+import com.gym.trainerService.Exception.Custom.UnAuthorizedRequestException;
 import com.gym.trainerService.Models.Review;
 import com.gym.trainerService.Models.Trainer;
 import com.gym.trainerService.Repositories.ReviewRepository;
 import com.gym.trainerService.Repositories.TrainerRepository;
+import com.gym.trainerService.Utils.CustomAnnotations.Annotations.LogRequestTime;
+import com.gym.trainerService.Utils.CustomCacheEvict;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -25,6 +31,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+
 /**
  * Service class responsible for handling all business logic associated
  * with trainer reviews, including adding, updating, deleting, and
@@ -58,6 +66,8 @@ public class TrainerReviewService {
      * and query operations.
      */
     private final ReviewRepository reviewRepository;
+    private final CacheManager cacheManager;
+    private final CustomCacheEvict evict;
 
     /**
      * Adds a new review for a specific trainer. This method retrieves the
@@ -72,19 +82,18 @@ public class TrainerReviewService {
      * @param requestDto the DTO containing review request data
      * @return a structured {@link ReviewResponseDto} representing the saved review
      */
+    @LogRequestTime
     @Transactional
     @Caching(evict = {
             @CacheEvict(value = "AllTrainerCache", key = "'All'"),
             @CacheEvict(value = "trainerCache", key = "#trainerId"),
-            @CacheEvict(value = "reviewCache", key = "#trainerId + '*' "),
+            @CacheEvict(value = "reviewCache", key = "#requestDto.userId"),
             @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
             @CacheEvict(value = "ratingMatrix" , key = "#trainerId")
     })
-    public ReviewResponseDto addReviewForMemberByUser(String trainerId, ReviewAddRequestDto requestDto) {
+    public String  addReviewForTrainerByUser(String trainerId, ReviewAddRequestDto requestDto) {
         // fetching the trainer by id from db (if valid) otherwise throws exception
-        Trainer trainer = trainerRepository.findById(trainerId)
-                .orElseThrow(() -> new NoTrainerFoundException(
-                        "No trainer found with the id: " + trainerId));
+        Trainer trainer = getById(trainerId);
         log.info("Successfully retrieved trainer from db with id {}", trainer.getTrainerId());
         // Building the review to save in the db by using builder pattern
         Review review = Review.builder()
@@ -97,13 +106,15 @@ public class TrainerReviewService {
                 .trainer(trainer)
                 .build();
         reviewRepository.save(review); // saving the review in the db
-        Double averageReview = reviewRepository.getReviewsByTrainerId(trainerId); // getting the avg review
-        if(trainer.getAverageRating() != averageReview) { // if the review is not equal only then change
-            trainer.setAverageRating(averageReview);     // otherwise not (less db interaction)
-            trainerRepository.save(trainer);
-        }
-        log.info("Successfully saved a review with id {} on {}", review.getReviewId(), LocalDateTime.now());
-        return reviewResponseBuilder(review);    // returning the response dto using a helper method
+        Double avg = reviewRepository.getReviewsByTrainerId(trainerId);
+        double newAvg = (avg != null) ? avg : 0.0;
+        trainerRepository.updateAverageRating(trainerId, newAvg);
+        evict.evictReviewCacheByTrainerId(trainerId);
+        log.info("Successfully saved a review for trainer {} at {}", trainerId, LocalDateTime.now());
+        return String.format(
+                "Thanks for your feedback! Your %.1f-star review has been added successfully.",
+                review.getReview()
+        );
     }
 
     /**
@@ -116,24 +127,30 @@ public class TrainerReviewService {
      * @param trainerId      the unique trainer identifier
      * @param pageNo         the page number to fetch
      * @param pageSize       the number of items per page
-     * @param sortBy         the field used for sorting
      * @param sortDirection  the direction of sorting, either {@code ASC} or {@code DESC}
      * @return an {@link AllReviewResponseWrapperDto} containing a list of review DTOs
      */
+    @LogRequestTime
     @Cacheable(
             value = "reviewCache",
-            key = "#trainerId + ':' + #pageNo + ':' + #pageSize + ':' + #sortBy + ':' + #sortDirection"
+            key = "#trainerId + ':' + #pageNo + ':' + #pageSize +  ':' + #sortDirection"
     )
     public AllReviewResponseWrapperDto getAllReviewByTrainerId(String trainerId, int pageNo, int pageSize,
-                                                               String sortBy, String sortDirection) {
+                                                               String sortDirection) {
         // building sort by using sortingBy and sorting direction(ascending/descending)
-        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Trainer trainer = getById(trainerId);
+        log.info("®️®️ request received to get review for trainer {} {} ",
+                trainer.getFirstName(),trainer.getLastName());
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("ASC") ?
+                Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction,"reviewDate");
         // building the page request by page size, page no and sort
-        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+        Pageable pageable = PageRequest.of(pageNo, pageSize,sort);
         log.info("request reached in service logic to get reviews by pageNo {} & pageSize {} ", pageNo,pageSize);
+
         // retrieving reviews from database in list dataType
-        List<Review> reviews = reviewRepository.findReviewsByTrainerId(trainerId, pageable).stream().toList();
-        log.info("Successfully fetched {} reviews from database for trainer id: {}", reviews.size(), trainerId);
+        Page<Review> reviews = reviewRepository.findReviewsByTrainerId(trainerId, pageable);
+        log.info("Successfully fetched {} reviews from database for trainer id: {}", reviews.getSize(), trainerId);
         // using stream and map to make reviewResponseDtoList to return it through AllReviewResponseWrapperDto
         List<ReviewResponseDto> reviewResponseDtoList = reviews.stream()
                 .map(review -> ReviewResponseDto.builder()
@@ -141,8 +158,6 @@ public class TrainerReviewService {
                         .userId(review.getUserId()).userName(review.getUserName())
                         .userRole(review.getUserRole())
                         .reviewDate(review.getReviewDate())
-                        .helpFullVote(review.getHelpFullVote())
-                        .notHelpFullVote(review.getNotHelpFullVote())
                         .comment(review.getComment())
                         .review(review.getReview())
                         .build())
@@ -151,9 +166,43 @@ public class TrainerReviewService {
         // returning the review
         return AllReviewResponseWrapperDto.builder()
                 .reviewResponseDtoList(reviewResponseDtoList)
+                .pageNo(reviews.getNumber())
+                .pageSize(reviews.getSize())
+                .totalElements(reviews.getTotalElements())
+                .totalPages(reviews.getTotalPages())
+                .lastPage(reviews.isLast())
                 .build();
     }
 
+    @LogRequestTime
+    @Cacheable(value = "reviewCache", key = "#userId")
+    public AllReviewResponseWrapperDto getReviewByUserId(String userId,int pageNo,int pageSize, String sortDirection) {
+        log.info("👔👔 request receive to get reviews for -->{}",userId);
+        Sort.Direction direction = sortDirection.equalsIgnoreCase("ASC") ?
+                Sort.Direction.ASC : Sort.Direction.DESC;
+        Pageable page = PageRequest.of(pageNo,pageSize,Sort.by(direction,"reviewDate"));
+        Page<Review> reviews = reviewRepository.findByUserId(userId,page);
+        List<ReviewResponseDto> reviewResponseDtoList = reviews.stream()
+                .map(review -> ReviewResponseDto.builder()
+                        .reviewId(review.getReviewId())
+                        .userId(review.getUserId()).userName(review.getUserName())
+                        .userRole(review.getUserRole())
+                        .reviewDate(review.getReviewDate())
+                        .comment(review.getComment())
+                        .review(review.getReview())
+                        .build())
+                .toList();
+        log.info("Successfully building review list for {} no of reviews for user ", reviewResponseDtoList.size());
+        // returning the review
+        return AllReviewResponseWrapperDto.builder()
+                .reviewResponseDtoList(reviewResponseDtoList)
+                .pageNo(reviews.getNumber())
+                .pageSize(reviews.getSize())
+                .totalElements(reviews.getTotalElements())
+                .totalPages(reviews.getTotalPages())
+                .lastPage(reviews.isLast())
+                .build();
+    }
     /**
      * Updates an existing trainer review based on its review ID and corresponding
      * request data. Ensures that user identity and trainer integrity match before
@@ -166,46 +215,47 @@ public class TrainerReviewService {
      * @param requestDto contains updated review data
      * @return the updated {@link ReviewResponseDto}
      */
+    @LogRequestTime
     @Transactional
     @Caching(evict = {
             @CacheEvict(value = "trainerCache", key = "#requestDto.trainerId"),
             @CacheEvict(value = "AllTrainerCache", key = "'All'"),
-            @CacheEvict(value = "reviewCache", key = "#requestDto.trainerId + '*'"),
+            @CacheEvict(value = "reviewCache", key = "#requestDto.userId"),
             @CacheEvict(value = "ratingMatrix" , key = "#requestDto.trainerId"),
-            @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
+            @CacheEvict(value = "DashboardInfo",key = "#requestDto.trainerId")
     })
     public ReviewResponseDto updateReviewForTrainerById(String reviewId, ReviewUpdateRequestDto requestDto) {
+
         Review review = reviewRepository.findById(reviewId)
-                .orElseThrow(() -> new NoReviewFoundException(
-                        "No review find with this id : " + reviewId));
-        Trainer trainer = trainerRepository.findById(review.getTrainer().getTrainerId())
-                .orElseThrow(() -> new NoTrainerFoundException(
-                        "No trainer found with the id: " + review.getTrainer().getTrainerId()));
-        boolean condition1 = review.getUserId().equals(requestDto.getUserId()) &&
-                review.getTrainer().getTrainerId().equals(requestDto.getTrainerId());
-        boolean condition2 = review.getUserName().equals(requestDto.getUserName()) &&
-                review.getUserRole().equals(requestDto.getUserRole());
+                .orElseThrow(() -> new NoReviewFoundException("No review found"));
 
-        if (condition1 && condition2) {
-            log.info("Fetched a valid review for trainer id {} by {} ",
-                    review.getTrainer().getTrainerId(), review.getUserName());
-            review.setReviewDate(requestDto.getReviewDate());
-            review.setComment(requestDto.getComment());
-            review.setReview(requestDto.getReview());
-            reviewRepository.save(review);
-            log.info("Successfully updated review for review id: {}", review.getReviewId());
-            Double averageReview = reviewRepository
-                    .getReviewsByTrainerId(review.getTrainer().getTrainerId());// getting the avg review
-            if(trainer.getAverageRating() != averageReview) { // if the review is not equal only then change
-                trainer.setAverageRating(averageReview);     // otherwise not (less db interaction)
-                trainerRepository.save(trainer);
-            }
-
-            return reviewResponseBuilder(review);
+        if (!review.getUserId().equals(requestDto.getUserId()) ||
+                !review.getTrainer().getTrainerId().equals(requestDto.getTrainerId()) ||
+                !review.getUserName().equals(requestDto.getUserName()) ||
+                !review.getUserRole().equals(requestDto.getUserRole()))
+        {
+            throw new InvalidReviewException("Invalid review credentials.");
         }
-        throw new InvalidReviewException("Invalid review found doesn't matches user role or user name " +
-                "or user id or trainer id ");
+
+        String tId = review.getTrainer().getTrainerId();
+
+        review.setReviewDate(requestDto.getReviewDate());
+        review.setComment(requestDto.getComment());
+        review.setReview(requestDto.getReview());
+        reviewRepository.save(review);
+
+        Double avg = reviewRepository.getReviewsByTrainerId(tId);
+        double newAvg = (avg != null) ? avg : 0.0;
+
+        trainerRepository.updateAverageRating(tId, newAvg);
+
+        evict.evictReviewCacheByTrainerId(tId);
+
+        log.info("Successfully updated review {}", reviewId);
+
+        return reviewResponseBuilder(review);
     }
+
 
     /**
      * Deletes a review identified by its ID for a given trainer.
@@ -218,30 +268,40 @@ public class TrainerReviewService {
      * @param trainerId the trainer associated with the review
      * @return confirmation message indicating successful deletion
      */
+    @LogRequestTime
     @Transactional
     @Caching(evict = {
             @CacheEvict(value = "trainerCache", key = "#trainerId"),
             @CacheEvict(value = "AllTrainerCache", key = "'All'"),
-            @CacheEvict(value = "reviewCache", key = "#trainerId + '*'"),
-            @CacheEvict(value = "ratingMatrix" , key = "#trainerId"),
-            @CacheEvict(value = "DashboardInfo",key = "#trainerId"),
+            @CacheEvict(value = "reviewCache", key = "#userId"),
+            @CacheEvict(value = "ratingMatrix", key = "#trainerId"),
+            @CacheEvict(value = "DashboardInfo", key = "#trainerId")
     })
-    public String deleteReviewForTrainerByReviewId(String reviewId, String trainerId) {
+    public String deleteReviewForTrainerByReviewId(String reviewId, String trainerId, String userId) {
+
+        Trainer trainer = getById(trainerId);
+        // 1) load & validate
         Review review = reviewRepository.findById(reviewId)
-                .orElseThrow(() -> new NoReviewFoundException(
-                        "No review find with this id : " + reviewId));
-        reviewRepository.deleteById(review.getReviewId());
-        Double averageRating = reviewRepository.getReviewsByTrainerId(review.getTrainer().getTrainerId());
-        Trainer trainer = trainerRepository.findById(review.getTrainer().getTrainerId())
-                .orElseThrow(() -> new NoTrainerFoundException(
-                        "No trainer found with the id: " + review.getTrainer().getTrainerId()));
-        if (averageRating.equals(trainer.getAverageRating())) {
-            trainer.setAverageRating(averageRating);
-            trainerRepository.save(trainer);
+                .orElseThrow(() -> new NoReviewFoundException("No review found"));
+        if (!review.getUserId().equals(userId)) {
+            throw new UnAuthorizedRequestException("You can only delete your own reviews");
         }
-        return "Successfully deleted review for review id: " + review.getReviewId() + "for trainer id: "
-                + trainerId;
+        if (!review.getTrainer().getTrainerId().equals(trainerId)) {
+            throw new NoTrainerFoundException("Trainer id mismatch in review");
+        }
+        String tId = review.getTrainer().getTrainerId();
+
+        reviewRepository.deleteById(reviewId);
+        reviewRepository.flush();
+        Double avg = reviewRepository.getReviewsByTrainerId(tId); // avg or null
+        double newAvg = (avg != null) ? avg : 0.0;
+        int effectedRows =  trainerRepository.updateAverageRating(tId, newAvg);
+        log.info("Deleted successfully and rows effected ==> {}",effectedRows);
+        evict.evictReviewCacheByTrainerId(tId);
+
+        return "Successfully deleted review for "+trainer.getFirstName()+" "+trainer.getLastName();
     }
+
 
     /**
      * Helper method that constructs a {@link ReviewResponseDto} instance
@@ -258,12 +318,11 @@ public class TrainerReviewService {
                 .userRole(review.getUserRole())
                 .reviewDate(review.getReviewDate())
                 .review(review.getReview())
-                .helpFullVote(review.getHelpFullVote())
-                .notHelpFullVote(review.getNotHelpFullVote())
                 .comment(review.getComment())
                 .build();
     }
 
+    @LogRequestTime
     @Cacheable(value = "ratingMatrix" , key = "#trainerId")
     public RatingMatrixInfo getRatingMatrix(String trainerId) {
         LocalDateTime now = LocalDateTime.now();
@@ -295,5 +354,31 @@ public class TrainerReviewService {
                 .oldRating(oldRating)
                 .change(percentageChange)
                 .build();
+    }
+
+    private Trainer getById(String trainerId){
+        Trainer trainer = null;
+
+        Cache.ValueWrapper wrapper = Objects.requireNonNull(cacheManager.getCache("trainer")).get(trainerId);
+
+        if (wrapper != null) {
+            try {
+                trainer = (Trainer) wrapper.get();
+                log.info("Trainer {} {} found in cache", Objects.requireNonNull(trainer).getFirstName(), trainer.getLastName());
+            } catch (ClassCastException e) {
+                log.warn("Error occurred during cache cast for trainerId {}: {}", trainerId, e.getLocalizedMessage());
+            }
+        }
+
+        if (trainer == null) {
+            trainer = trainerRepository.findById(trainerId)
+                    .orElseThrow(()-> new NoTrainerFoundException("No Trainer Found with this id"));
+
+            log.info("Reloaded trainer {} {} from db", trainer.getFirstName(), trainer.getLastName());
+            Objects.requireNonNull(cacheManager.getCache("trainer")).put(trainer.getTrainerId(), trainer);
+        }
+
+        log.info("review service returning {} {} by private method", trainer.getFirstName(), trainer.getLastName());
+        return trainer;
     }
 }
