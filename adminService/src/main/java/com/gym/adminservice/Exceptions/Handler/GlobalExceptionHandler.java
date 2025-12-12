b22c49a1dc7174779f99815bae471dd9cf881c52
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-12 19:37:26 +0530

    handled the exceptions in the global handler

--- adminService/src/main/java/com/gym/adminservice/Exceptions/Handler/GlobalExceptionHandler.java
+++ adminService/src/main/java/com/gym/adminservice/Exceptions/Handler/GlobalExceptionHandler.java
@@ -1,14 +1,12 @@
 package com.gym.adminservice.Exceptions.Handler;
 
-import com.gym.adminservice.Exceptions.Custom.InvalidUserException;
-import com.gym.adminservice.Exceptions.Custom.MessageNotFoundException;
-import com.gym.adminservice.Exceptions.Custom.PlanNotFounException;
-import com.gym.adminservice.Exceptions.Custom.RequestNotFoundException;
+import com.gym.adminservice.Exceptions.Custom.*;
 import com.gym.adminservice.Exceptions.Model.ErrorResponse;
 import com.gym.adminservice.Exceptions.Utils.ExceptionUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -27,13 +25,15 @@ public class GlobalExceptionHandler {
     ResponseEntity<ErrorResponse> handleWebClient(Exception ex, HttpServletRequest request) {
         return ExceptionUtil.buildErrorResponse(HttpStatus.SERVICE_UNAVAILABLE, ex.getMessage(), request);
     }
-    @ExceptionHandler({Exception.class})
+    @ExceptionHandler({InvalidUserException.class, MethodArgumentNotValidException.class,
+            MessageLimitReachedException.class,UnauthorizedRequestException.class})
+    ResponseEntity<ErrorResponse> handleInvalidRequests(Exception ex, HttpServletRequest request){
+        return ExceptionUtil.buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
+    }
+    @ExceptionHandler({Exception.class, interServiceCommunicationException.class})
     ResponseEntity<ErrorResponse> handleGlobal(Exception ex, HttpServletRequest request) {
         return ExceptionUtil.buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,ex.getMessage(),request);
     }
 
-    @ExceptionHandler({InvalidUserException.class})
-    ResponseEntity<ErrorResponse> handleInvalidRequests(Exception ex, HttpServletRequest request){
-        return ExceptionUtil.buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
-    }
+
 }
