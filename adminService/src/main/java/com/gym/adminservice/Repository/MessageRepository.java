7e4e0698b54ed94b246bb7511ec4e6ed38fc0857
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-15 20:29:20 +0530

    add methods to findout filterd messages/reports as per admin's requirements

--- adminService/src/main/java/com/gym/adminservice/Repository/MessageRepository.java
+++ adminService/src/main/java/com/gym/adminservice/Repository/MessageRepository.java
@@ -2,6 +2,8 @@ package com.gym.adminservice.Repository;
 
 import com.gym.adminservice.Models.Messages;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,4 +15,15 @@ public interface MessageRepository extends JpaRepository<Messages,String> {
 
     @Query("SELECT count (m) FROM Messages m WHERE m.userId =:userId")
     int countByUserId(@Param("userId") String userId);
+
+    @Query("""
+                SELECT m FROM Messages m
+                WHERE (:role = 'ALL' OR m.userRole = :role)
+                  AND (:status = 'ALL' OR m.status = :status)
+            """)
+    Page<Messages> findAllWithFilters(
+            @Param("role") String role,
+            @Param("status") String status,
+            Pageable pageable
+    );
 }
