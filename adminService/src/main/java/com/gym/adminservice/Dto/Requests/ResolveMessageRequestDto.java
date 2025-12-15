e98e1079bed4151a1d5bc967ea08e17371cf4128
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-15 20:25:55 +0530

    update the feild

--- adminService/src/main/java/com/gym/adminservice/Dto/Requests/ResolveMessageRequestDto.java
+++ adminService/src/main/java/com/gym/adminservice/Dto/Requests/ResolveMessageRequestDto.java
@@ -1,5 +1,7 @@
 package com.gym.adminservice.Dto.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +12,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ResolveMessageRequestDto {
+    @NotBlank(message = "requestId is required")
     private String requestId;
+
+    @Builder.Default
+    private boolean notify = false;
+
+    @Size(max = 500, message = "mailMessage cannot be longer than 500 characters")
     private String mailMessage;
-    private boolean delete;
+
+    @Builder.Default
+    private boolean decline = false;
 }
