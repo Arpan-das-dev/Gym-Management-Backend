2f3ebd345dfdd19e1ac265ea0851d6a20ce3bea3
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-15 20:32:31 +0530

    update the method generate reportId

--- adminService/src/main/java/com/gym/adminservice/Utils/ReportIdGenUtil.java
+++ adminService/src/main/java/com/gym/adminservice/Utils/ReportIdGenUtil.java
@@ -3,11 +3,17 @@ package com.gym.adminservice.Utils;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class ReportIdGenUtil {
-    public String createMessageId(String userId, LocalDateTime messageTime){
-        return userId+messageTime.toString()+ ThreadLocalRandom.current().toString();
+    public String createMessageId(String userId, LocalDateTime time){
+        return "MSG-" +
+                time.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")) +
+                "-" +
+                userId.substring(0, Math.min(6, userId.length())) +
+                "-" +
+                ThreadLocalRandom.current().nextInt(1000, 9999);
     }
 }
