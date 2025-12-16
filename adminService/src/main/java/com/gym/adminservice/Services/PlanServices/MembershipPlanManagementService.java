0ec77efcee01ef05ce4d84e8f07c601f5ba52170
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-16 19:33:28 +0530

    add logs

--- adminService/src/main/java/com/gym/adminservice/Services/PlanServices/MembershipPlanManagementService.java
+++ adminService/src/main/java/com/gym/adminservice/Services/PlanServices/MembershipPlanManagementService.java
@@ -5,6 +5,7 @@ import com.gym.adminservice.Dto.PlanDtos.Requests.UpdatePlanRequestDto;
 import com.gym.adminservice.Dto.PlanDtos.Responses.CreationResponseDto;
 import com.gym.adminservice.Exceptions.Custom.PlanNotFounException;
 import com.gym.adminservice.Services.WebClientServices.WebClientPlanService;
+import com.gym.adminservice.Utils.CustomAnnotations.Annotations.LogRequestTime;
 import com.gym.adminservice.Utils.PlanIdGenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,7 @@ public class MembershipPlanManagementService {
     private final PlanIdGenUtil planIdGenUtil;
     private final WebClientPlanService webClientPlanService;
 
-
+    @LogRequestTime
     public String createPlan(PlanCreateRequestDto requestDto) throws ExecutionException, InterruptedException {
         String customId = planIdGenUtil.generatePlanId(
                 requestDto.getPlanName(), requestDto.getDuration(), requestDto.getPrice()
@@ -36,27 +37,27 @@ public class MembershipPlanManagementService {
                 .features(requestDto.getFeatures())
                 .build();
 
-        String response =  webClientPlanService.sendCreationToPlanService(responseDto).get();
-        log.info("response received from plan service to create plan ::{}",response);
+        String response = webClientPlanService.sendCreationToPlanService(responseDto).get();
+        log.info("response received from plan service to create plan ::{}", response);
         return response;
     }
 
-
+    @LogRequestTime
     public List<CreationResponseDto> getAllPlans() {
         return webClientPlanService.getAllPlansFromPlanService();
     }
 
-
-    public String  updatePlan(String id, UpdatePlanRequestDto requestDto) throws ExecutionException, InterruptedException {
-        String response =  webClientPlanService.sendUpdateCreationToPlanService(id,requestDto).get();
-        log.info("response received from plan service to update plan ::{}",response);
+    @LogRequestTime
+    public String updatePlan(String id, UpdatePlanRequestDto requestDto) throws ExecutionException, InterruptedException {
+        String response = webClientPlanService.sendUpdateCreationToPlanService(id, requestDto).get();
+        log.info("response received from plan service to update plan ::{}", response);
         return response;
     }
 
-
-    public String  deletePlan(String id) throws ExecutionException, InterruptedException, PlanNotFounException {
-        String response =  webClientPlanService.sendDeletionRequestById(id).get();
-        log.info("response received from plan service to delete plan ::{}",response);
+    @LogRequestTime
+    public String deletePlan(String id) throws ExecutionException, InterruptedException, PlanNotFounException {
+        String response = webClientPlanService.sendDeletionRequestById(id).get();
+        log.info("response received from plan service to delete plan ::{}", response);
         return response;
     }
 
