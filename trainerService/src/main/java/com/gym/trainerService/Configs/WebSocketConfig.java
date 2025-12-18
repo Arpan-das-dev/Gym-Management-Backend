272dd147e528463aa2fed5fa206949c496bb6881
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-18 18:20:27 +0530

    update the websocket config of trainer

--- trainerService/src/main/java/com/gym/trainerService/Configs/WebSocketConfig.java
+++ trainerService/src/main/java/com/gym/trainerService/Configs/WebSocketConfig.java
@@ -18,8 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .setAllowedOrigins(allowedOrigin);
+        registry.addEndpoint("/ws/trainers")
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
