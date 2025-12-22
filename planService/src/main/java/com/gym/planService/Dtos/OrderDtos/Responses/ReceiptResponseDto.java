685063a6cac0487f0624dc87cedc781c45a2fea5
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:36:33 +0530

    add dto to send about information of user's transactions

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/ReceiptResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/ReceiptResponseDto.java
@@ -1,4 +1,20 @@
 package com.gym.planService.Dtos.OrderDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceiptResponseDto {
+    private String planName;
+    private Double paidPrice;
+    private LocalDate paymentDate;
+    private String status;
+    private String receiptUrl;
 }
