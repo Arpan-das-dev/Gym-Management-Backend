a49455b897806f1847e6a71b0c8bafb561e41c21
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:16:25 +0530

    update method to get active user's count for a particular plan

---
+++ planService/src/main/java/com/gym/planService/Exception/Custom/PaymentFailedException.java
@@ -0,0 +1,7 @@
+package com.gym.planService.Exception.Custom;
+
+public class PaymentFailedException extends RuntimeException {
+  public PaymentFailedException(String message) {
+    super(message);
+  }
+}
