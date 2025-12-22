b903438c13387637a32d43ddbe9637b66a2c6b46
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:37:36 +0530

    add dto to send to notification service to let user know about the payment failed

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/PaymentFailedDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Responses/PaymentFailedDto.java
@@ -1,4 +1,18 @@
 package com.gym.planService.Dtos.OrderDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentFailedDto {
+    private String subject;
+    private String cause;
+    private Double amount;
+    private String userName;
+    private String paymentId;
 }
