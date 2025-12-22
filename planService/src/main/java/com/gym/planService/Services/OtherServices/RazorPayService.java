5dbc3ec1e9cb2961521969c9e8ea2b2e3774fb37
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:22:59 +0530

    add new method to refund user if payment failed

--- planService/src/main/java/com/gym/planService/Services/OtherServices/RazorPayService.java
+++ planService/src/main/java/com/gym/planService/Services/OtherServices/RazorPayService.java
@@ -3,6 +3,7 @@ package com.gym.planService.Services.OtherServices;
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
 import com.razorpay.RazorpayException;
+import com.razorpay.Refund;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
@@ -24,4 +25,14 @@ public class RazorPayService {
         return razorpayClient.orders.create(orderRequest);
     }
 
+    public String refundPayment(String paymentId, double amount, String reason) throws RazorpayException {
+        JSONObject refundRequest = new JSONObject();
+        refundRequest.put("amount", (int)(amount * 100)); // amount in paise
+        refundRequest.put("speed", "normal");
+        refundRequest.put("notes", new JSONObject().put("reason", reason));
+
+        Refund refund = razorpayClient.payments.refund(paymentId, refundRequest);
+        log.info("Refund initiated for paymentId: {} | RefundId: {}", paymentId, refund.get("id"));
+        return refund.get("id");
+    }
 }
