360ae334cb9fc616d4ef0ee1e64457043d71361b
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:35:02 +0530

    add new dto to send users a list of @ReceiptResponseDto.java with information of paginations

--- planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Wrappers/ReceiptResponseWrapperDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/OrderDtos/Wrappers/ReceiptResponseWrapperDto.java
@@ -1,4 +1,23 @@
 package com.gym.planService.Dtos.OrderDtos.Wrappers;
 
+import com.gym.planService.Dtos.OrderDtos.Responses.ReceiptResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReceiptResponseWrapperDto {
+    private List<ReceiptResponseDto> responseDtoList;
+    private int pageNo;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+
+    private boolean lastPage;
 }
