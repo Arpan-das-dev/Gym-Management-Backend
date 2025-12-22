c5ff171b5693bb8bc1565f15acc4b0f39d399005
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:27:07 +0530

    add new exception to tell user that refund from razorpay service is refunded

--- planService/src/main/java/com/gym/planService/Exception/Custom/RefundFailedException.java
+++ planService/src/main/java/com/gym/planService/Exception/Custom/RefundFailedException.java
@@ -1,7 +1,7 @@
 package com.gym.planService.Exception.Custom;
 
 public class RefundFailedException extends RuntimeException {
-  public RefundFailedException(String message) {
-    super(message);
-  }
+    public RefundFailedException(String message) {
+        super(message);
+    }
 }
