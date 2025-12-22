6295a49eb892560dc2b109cc46a1d2b1e593e8fc
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-22 20:32:08 +0530

    add new dto to send most popular planids

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/MostPopularPlanIds.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/MostPopularPlanIds.java
@@ -1,4 +1,16 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MostPopularPlanIds {
+    List<String> planIds;
 }
