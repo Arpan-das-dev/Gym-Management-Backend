093e5dd963e8906e3e541e3b029c98edcd93f7e2
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-23 21:53:04 +0530

    add new dtos to send response to admin for revenue and matrix details

--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanLifeTimeIncome.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/PlanLifeTimeIncome.java
@@ -1,4 +1,15 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlanLifeTimeIncome {
+    private Double revenue;
+    private Integer usage;
 }
--- planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/RevenueGeneratedPerPlanResponseDto.java
+++ planService/src/main/java/com/gym/planService/Dtos/PlanDtos/Responses/RevenueGeneratedPerPlanResponseDto.java
@@ -1,4 +1,16 @@
 package com.gym.planService.Dtos.PlanDtos.Responses;
 
-public class RevnueGeneretedPerPlanResponseDto {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RevenueGeneratedPerPlanResponseDto {
+    Map<String ,PlanLifeTimeIncome> allPlanIncomes;
 }
