def1f2e46761943def2837c109bcdec74e6efe83
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-27 20:58:37 +0530

    add new request dto to inform trainer about their freeze account information

--- notificationService/src/main/java/com/gym/notificationservice/Dto/MailNotificationDtos/FreezeTrainerRequestDto.java
+++ notificationService/src/main/java/com/gym/notificationservice/Dto/MailNotificationDtos/FreezeTrainerRequestDto.java
@@ -1,4 +1,18 @@
 package com.gym.notificationservice.Dto.MailNotificationDtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class FreezeTrainerRequestDto {
+    private String trainerName;
+    private String trainerMail;
+    private String subject;
+    private boolean frozen;
+    private String time;
 }
