08e8b6a41f67fe6ce1d819196e10cf8f94d0220f
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-28 18:02:41 +0530

    add validation

--- notificationService/src/main/java/com/gym/notificationservice/Dto/MailNotificationDtos/FreezeTrainerRequestDto.java
+++ notificationService/src/main/java/com/gym/notificationservice/Dto/MailNotificationDtos/FreezeTrainerRequestDto.java
@@ -1,5 +1,8 @@
 package com.gym.notificationservice.Dto.MailNotificationDtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +13,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FreezeTrainerRequestDto {
+    @NotBlank(message = "Trainer Name Can not be Blank")
     private String trainerName;
+    @NotBlank(message = "Please Provide a Valid Trainer's Email")
+    @Email(message = "Please Provide Mail in Valid Form")
     private String trainerMail;
+    @NotBlank(message = "Unable To Proceed Without having A valid Subject")
     private String subject;
+    @NotNull(message = "frozen Status Can not be Null")
     private boolean frozen;
+    @NotBlank(message = "Please Provide the time ")
     private String time;
 }
