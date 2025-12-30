8695a03888a6642c95ad3471aefc34bb5908789b
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-30 13:23:59 +0530

    add ratelimmiter package but due to some restriction we are not able to implement it

--- authService/src/main/java/com/gym/authservice/Config/RateLimmiter/RateLimitFilter.java
+++ authService/src/main/java/com/gym/authservice/Config/RateLimmiter/RateLimitFilter.java
@@ -1,4 +1,43 @@
 package com.gym.authservice.Config.RateLimmiter;
 
-public class RateLimitFilter {
-}
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+//@Component
+//@RequiredArgsConstructor
+//public class RateLimitFilter implements GlobalFilter, Ordered {
+//    private final TokenBucketRateLimiter rateLimiter;
+//
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        String key = resolveKey(exchange);
+//        return rateLimiter.allowRequest(key)
+//                .flatMap(allowed-> {
+//                    if(!allowed){
+//                        exchange.getResponse().setStatusCode(HttpStatus.TOO_MANY_REQUESTS);
+//                        return exchange.getResponse().setComplete();
+//                    }
+//                    return chain.filter(exchange);
+//                });
+//    }
+//
+//    private String resolveKey(ServerWebExchange exchange) {
+//        return Objects.requireNonNull(exchange.getRequest()
+//                        .getRemoteAddress())
+//                .getAddress()
+//                .getHostAddress();
+//    }
+//
+//    @Override
+//    public int getOrder() {
+//        return -100;
+//    }
+//}
--- authService/src/main/java/com/gym/authservice/Config/RateLimmiter/TokenBucketRateLimiter.java
+++ authService/src/main/java/com/gym/authservice/Config/RateLimmiter/TokenBucketRateLimiter.java
@@ -1,4 +1,63 @@
 package com.gym.authservice.Config.RateLimmiter;
 
-public class TokenBucketRateLimiter {
-}
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Map;
+
+//@Slf4j
+//@Service
+//public class TokenBucketRateLimiter {
+//    private final Double BURST_CAPACITY;
+//    private final Double REFILL_RATE;
+//    private final ReactiveRedisTemplate<String , String > redisTemplate;
+//
+//    public TokenBucketRateLimiter(@Value("${rateLimit.capacity}") Double BURST_CAPACITY,
+//                                  @Value("${rateLimit.fillRate}") Double REFILL_RATE,
+//                                  @Qualifier("rateLimitRedisTemplate")  ReactiveRedisTemplate<String, String> redisTemplate)
+//    {
+//        this.BURST_CAPACITY = BURST_CAPACITY;
+//        this.REFILL_RATE = REFILL_RATE;
+//        this.redisTemplate = redisTemplate;
+//    }
+//
+//    public Mono<Boolean> allowRequest (String key) {
+//        String redisKey = "rate:limit:"+key;
+//        long now = System.currentTimeMillis();
+//        return redisTemplate.opsForHash()
+//                .entries(redisKey)
+//                .collectMap(
+//                        e-> e.getKey().toString(),
+//                e-> e.getValue().toString())
+//                .defaultIfEmpty(Map.of())
+//                .flatMap(state-> {
+//                    double tokens = state.containsKey("tokens") ?
+//                            Double.parseDouble(state.get("tokens"))
+//                            : BURST_CAPACITY;
+//                    long latestRefills = state.containsKey("latestRefills") ?
+//                            Long.parseLong(state.get("latestRefills"))
+//                            : now;
+//
+//                    double refillTokens = ((now-latestRefills) / 1000.0) * REFILL_RATE;
+//                    tokens = Math.min(BURST_CAPACITY,tokens+refillTokens);
+//                    if(tokens<1) {
+//                        return Mono.just(false);
+//                    }
+//
+//                    double consumedToken = tokens -1;
+//                    Map<String , String > updateState = Map.of(
+//                            "tokens",String.valueOf(consumedToken),
+//                            "latestRefills", String.valueOf(now)
+//                    );
+//                    return redisTemplate.opsForHash()
+//                            .putAll(redisKey,updateState)
+//                            .then(redisTemplate.expire(redisKey, Duration.ofMinutes(10)))
+//                            .thenReturn(true);
+//                });
+//    }
+//}
